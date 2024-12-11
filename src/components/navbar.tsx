cb9a9cf3f9847230e63345649d67c569a7849ce0
Luis Viera <levieraf@gmail.com>
2024-12-11 12:35:32 -0500

    wip

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,17 +3,17 @@ import Link from "@/components/link";
 
 export default function Navbar() {
     return (
-        <nav className="flex justify-start items-center flex-row gap-2">
+        <nav className="flex justify-start items-center flex-row gap-3.5">
             <Link href="#">
                 Login
             </Link>
-            <Link className="px-3" href="#">
+            <Link className="py-[15px] px-[18px] leading-none" href="#">
                 <Image
                     src="/assets/images/menu-more.svg"
                     alt="Menu More"
                     aria-label="Menu More"
-                    width={24}
-                    height={24}
+                    width={16}
+                    height={4}
                 />
             </Link>
         </nav>
