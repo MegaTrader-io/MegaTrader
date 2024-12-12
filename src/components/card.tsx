f0985510a87387f980cccdb0cca370b315af2cdd
Luis Viera <levieraf@gmail.com>
2024-12-12 15:29:40 -0500

    wip

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -5,7 +5,7 @@ export default function Card({children, className = ''}: {
     children: React.ReactNode,
     className?: string
 }) {
-    return <div className={`p-4  bg-[#1e1e1e]/70 border border-transparent rounded-2xl  ${className}`}>
+    return <div className={`p-4 bg-[#1e1e1e]/70 border border-neutral-700 rounded-2xl  ${className}`}>
         {children || ''}
     </div>
 }
