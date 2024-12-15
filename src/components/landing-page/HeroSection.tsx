85f6b8d3d3c7fbfe075f67c26cef1e9a3f6602c9
Luis Viera <levieraf@gmail.com>
2024-12-14 19:48:01 -0500

    wip

--- src/components/landing-page/HeroSection.tsx
+++ src/components/landing-page/HeroSection.tsx
@@ -28,8 +28,7 @@ const HeroSection = () => (
         </div>
         <div className="w-full">
             <video className="w-full p-4" autoPlay loop muted playsInline width="auto">
-                <source src="/assets/video.mov"  type="video/mp4; codecs=hevc"/>
-                <source src="/assets/video.mov"  type="video/quicktime"/>
+                <source src="/assets/principal3.mp4" type="video/quicktime"/>
                 <source src="/assets/video.webm" type="video/webm"/>
                 Your browser does not support this video.
             </video>
