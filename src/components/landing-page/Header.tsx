a2c2c47274fcc50a28451ef5ba2faee41b25fce9
Luis Viera <levieraf@gmail.com>
2024-12-20 20:54:39 -0500

    wip

--- src/components/landing-page/Header.tsx
+++ src/components/landing-page/Header.tsx
@@ -59,7 +59,7 @@ export default function Header() {
         e.preventDefault();
         const element = document.querySelector(href);
         if (element) {
-            const headerOffset = 131;
+            const headerOffset = 131+32;
             const elementPosition = element.getBoundingClientRect().top + window.scrollY;
             const offsetPosition = elementPosition - headerOffset;
 
