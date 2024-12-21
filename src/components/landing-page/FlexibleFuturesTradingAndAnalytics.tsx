ee5c9d51d2917ae8224d3c145190d8e0b8613f40
Luis Viera <levieraf@gmail.com>
2024-12-21 18:15:42 -0500

    wip

--- src/components/landing-page/FlexibleFuturesTradingAndAnalytics.tsx
+++ src/components/landing-page/FlexibleFuturesTradingAndAnalytics.tsx
@@ -106,52 +106,50 @@ const FlexibleFuturesTradingAndAnalytics = () => {
 
     return (
         <section className="mb-8 w-full px-16 bg-gradient-to-b from-[#1e1e1e] to-[#131210] rounded-2xl">
-            <h2 className="text-5xl text-white text-center font-light leading-[60px]">
+            <h2 className="text-5xl text-white text-center font-light leading-[60px] py-8">
                 FLEXIBLE FUTURES<br/>
                 <span className="text-mgt-primary leading-[60px]">TRADING AND ANALYTICS</span>
             </h2>
-            <div>
-                <TabGroup onChange={setSelectedIndex}>
-                    <TabList className="flex gap-2 py-8">
-                        {Options.map((option, index) => (
-                            <Tab
-                                key={option.id}
-                                className="text-left py-3 px-4 text-stone-400 w-full rounded-xl border border-stone-400  focus:outline-none data-[selected]:bg-[#f1a035] data-[selected]:border-transparent data-[selected]:text-black">
-                                <div className="flex gap-2 items-center font-normal text-nowrap leading-6 text-base">
-                                    {selectedIndex === index &&
-                                        <CheckCircleIcon aria-hidden="true"
-                                                         className="w-6 h-6 text-black"/>} {option.title}
-                                </div>
-                            </Tab>
-                        ))}
-                    </TabList>
-                    <TabPanels
-                        className="h-[396px] w-full bg-[#1e1e1e] rounded-2xl shadow-[0px_20px_20px_20px_rgba(0,0,0,0.10)] justify-start items-center inline-flex overflow-hidden">
-                        {Options.map(option => (
-                            <TabPanel key={option.id} className="grid grid-cols-[511px_1fr] items-center">
-                                <div className="p-8 space-y-8">
-                                    <h2 className="text-white text-2xl font-light uppercase leading-73123">{option.panel.title}</h2>
-                                    <p className="text-stone-400 text-xl font-normal leading-8">
-                                        {option.panel.content}
-                                    </p>
+            <TabGroup onChange={setSelectedIndex}>
+                <TabList className="flex gap-2 pb-8">
+                    {Options.map((option, index) => (
+                        <Tab
+                            key={option.id}
+                            className="text-left py-3 px-4 text-stone-400 w-full rounded-xl border border-stone-400  focus:outline-none data-[selected]:bg-[#f1a035] data-[selected]:border-transparent data-[selected]:text-black">
+                            <div className="flex gap-2 items-center font-normal text-nowrap leading-6 text-base">
+                                {selectedIndex === index &&
+                                    <CheckCircleIcon aria-hidden="true"
+                                                     className="w-6 h-6 text-black"/>} {option.title}
+                            </div>
+                        </Tab>
+                    ))}
+                </TabList>
+                <TabPanels
+                    className="h-[396px] w-full bg-[#1e1e1e] rounded-2xl shadow-[0px_20px_20px_20px_rgba(0,0,0,0.10)] justify-start items-center inline-flex overflow-hidden">
+                    {Options.map(option => (
+                        <TabPanel key={option.id} className="grid grid-cols-[511px_1fr] items-center">
+                            <div className="p-8 space-y-8">
+                                <h2 className="text-white text-2xl font-light uppercase leading-73123">{option.panel.title}</h2>
+                                <p className="text-stone-400 text-xl font-normal leading-8">
+                                    {option.panel.content}
+                                </p>
 
-                                    <Link href={option.panel.button.href} className="bg-mgt-primary !text-black">
-                                        {option.panel.button.text}
-                                    </Link>
-                                </div>
-                                <div>
-                                    <Image
-                                        src={option.panel.image.url}
-                                        alt={option.panel.title}
-                                        width={641}
-                                        height={396}
-                                    />
-                                </div>
-                            </TabPanel>
-                        ))}
-                    </TabPanels>
-                </TabGroup>
-            </div>
+                                <Link href={option.panel.button.href} className="bg-mgt-primary !text-black">
+                                    {option.panel.button.text}
+                                </Link>
+                            </div>
+                            <div>
+                                <Image
+                                    src={option.panel.image.url}
+                                    alt={option.panel.title}
+                                    width={641}
+                                    height={396}
+                                />
+                            </div>
+                        </TabPanel>
+                    ))}
+                </TabPanels>
+            </TabGroup>
         </section>
     )
 }
