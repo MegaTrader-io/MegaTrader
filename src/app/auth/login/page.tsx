aaa6d62f1da36926e99bc48a1432bad502fe4b49
Luis Viera <levieraf@gmail.com>
2025-01-09 15:33:24 -0500

    wip

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,97 +1,207 @@
+'use client';
+
 import InputText from "@/components/ui/input-text";
 import {InputCheckbox} from "@/components/ui/input-checkbox";
 import Link from "next/link";
 import Badge from "@/components/ui/badge";
 import Image from "next/image";
+import {useState, useEffect} from "react";
 
-export default async function Login() {
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [fieldErrors, setFieldErrors] = useState<Record<string, string>>({});
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [debouncedEmail, setDebouncedEmail] = useState(email);
 
-    return <div className="flex flex-1">
-        <div className="flex w-full lg:w-2/5 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-            <div className="mx-auto w-full max-w-sm lg:w-96">
-                <div className="mt-10">
-                    <div>
-                        <div className="w-full mx-auto">
-                            <h1 className="text-white text-5xl font-light uppercase leading-[60px] mb-2">
-                                SIGN IN
-                            </h1>
-                            <h2
-                                className={`text-stone-400 text-base font-normal font-roboto leading-normal tracking-wide`}>Dont
-                                Welcome
-                                back! Please enter your details.
-                            </h2>
+    function validateEmail(email: string): boolean {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
 
-                            <form action="#" method="POST" className="space-y-4 lg:my-8">
-                                <div>
-                                    <InputText placeholder="Email" name="email" type="email"/>
-                                </div>
-                                <div>
-                                    <InputText placeholder="Password" name="password" type="password"/>
-                                </div>
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedEmail(email);
+        }, 500);
 
-                                <div className="flex items-center justify-between">
-                                    <InputCheckbox className="text-base"
-                                                   label="Remember me"
-                                                   value="1"
-                                                   name="remember"/>
-                                    <Link href="#"
-                                          className="text-base btn-link">
-                                        Forgot Password?
+        return () => clearTimeout(handler);
+    }, [email]);
+
+    useEffect(() => {
+        if (!debouncedEmail) {
+            setEmailError("");
+            return;
+        }
+
+        if (!validateEmail(debouncedEmail)) {
+            setEmailError("Ups... The email is not correct");
+        } else {
+            setEmailError("");
+        }
+    }, [debouncedEmail]);
+
+    function submitForm(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        if (!email.trim()) {
+            setEmailError("The email field is required");
+            return;
+        }
+
+        if (!password.trim()) {
+            setEmailError("The email field is required");
+            return;
+        }
+
+        if (!validateEmail(email)) {
+            setEmailError("Ups... The email is not correct");
+            return;
+        }
+
+        if (!email || !password) {
+            setFieldErrors({form: "Both fields are required"});
+            return;
+        }
+
+        setIsSubmitting(true);
+        setFieldErrors({});
+
+        setTimeout(() => {
+            setIsSubmitting(false);
+
+            setFieldErrors({
+                email: "These credentials do not match our records.",
+            });
+        }, 2000);
+    }
+
+    // const isFormValid = email && password && !emailError;
+
+    return (
+        <div className="flex flex-1">
+            <div
+                className="flex w-full lg:w-2/5 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+                <div className="mx-auto w-full max-w-sm lg:w-96">
+                    <div className="mt-10">
+                        <div>
+                            <div className="w-full mx-auto">
+                                <h1 className="text-white text-5xl font-light uppercase leading-[60px] mb-2">
+                                    SIGN IN
+                                </h1>
+                                <h2
+                                    className="text-stone-400 text-base font-normal font-roboto leading-normal tracking-wide"
+                                >
+                                    Welcome back! Please enter your details.
+                                </h2>
+
+                                <form onSubmit={submitForm} className="space-y-4 lg:my-8">
+                                    <div>
+                                        <InputText
+                                            type="email"
+                                            placeholder="Email"
+                                            name="email"
+                                            value={email}
+                                            onChange={(e) => {
+                                                const value = e.target.value;
+                                                setEmail(value);
+
+                                                if (!value) {
+                                                    setEmailError("");
+                                                    setFieldErrors((prev) => ({...prev, email: ""}));
+                                                    return;
+                                                }
+                                            }}
+                                            errorMessage={emailError || fieldErrors.email}
+                                        />
+                                    </div>
+                                    <div>
+                                        <InputText
+                                            type="password"
+                                            placeholder="Password"
+                                            name="password"
+                                            value={password}
+                                            onChange={(e) => {
+                                                setPassword(e.target.value);
+                                            }}
+                                        />
+                                    </div>
+
+                                    {fieldErrors.form && (
+                                        <div className="text-red-500 text-sm mt-2">{fieldErrors.form}</div>
+                                    )}
+
+                                    <div className="flex items-center justify-between">
+                                        <InputCheckbox
+                                            className="text-base"
+                                            label="Remember me"
+                                            value="1"
+                                            name="remember"
+                                        />
+                                        <Link href="#" className="text-base btn-link">
+                                            Forgot Password?
+                                        </Link>
+                                    </div>
+
+                                    <button
+                                        type="submit"
+                                        disabled={isSubmitting}
+                                        className={`h-12 w-full px-4 disabled:opacity-30 py-3 rounded-xl border-2 justify-center items-center gap-2 inline-flex ${
+                                            isSubmitting
+                                                ? "bg-gray-500 border-gray-500 cursor-not-allowed"
+                                                : "bg-mgt-primary border-mgt-primary"
+                                        }`}
+                                    >
+                                        <div
+                                            className="text-slate-950 text-base font-normal uppercase leading-normal"
+                                        >
+                                            {isSubmitting ? "Loading..." : "Sign In"}
+                                        </div>
+                                    </button>
+                                </form>
+
+                                <div className="text-center space-y-2">
+                                    <Link
+                                        href="#"
+                                        className="text-center w-full text-stone-400 text-base font-normal font-roboto leading-normal tracking-wide"
+                                    >
+                                        Don’t have an account?
                                     </Link>
+
+                                    <button
+                                        className="h-12 w-full px-4 py-3 bg-stone-800 rounded-xl border border-neutral-700 justify-center items-center gap-2 inline-flex">
+                                        <div className="text-neutral-50 text-base font-normal uppercase leading-normal">
+                                            Create account
+                                        </div>
+                                    </button>
                                 </div>
-
-                                <button
-                                    className="h-12 w-full px-4 py-3 bg-mgt-primary rounded-xl border-2 border-mgt-primary justify-center items-center gap-2 inline-flex">
-                                    <div
-                                        className="text-slate-950 text-base font-normal uppercase leading-normal">Sign
-                                        In
-                                    </div>
-                                </button>
-                            </form>
-
-                            <div className="text-center space-y-2">
-                                <Link href="#"
-                                      className={`text-center w-full text-stone-400 text-base font-normal font-roboto leading-normal tracking-wide`}>Don{'\''}t
-                                    have an account?
-                                </Link>
-
-                                <button
-                                    className="h-12 w-full  px-4 py-3 bg-stone-800 rounded-xl border border-neutral-700 justify-center items-center gap-2 inline-flex">
-                                    <div
-                                        className="text-neutral-50 text-base font-normal uppercase leading-normal">Create
-                                        account
-                                    </div>
-                                </button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div className="relative hidden w-0 flex-1 lg:block bg-[#1e1e1e] pl-[120px] overflow-hidden">
-            <Badge className="mt-[91px] mb-[17px]">
-                Start earning up to 90% profit
-            </Badge>
-            <p className="text-stone-400 text-base font-normal leading-normal max-w-[434px]">No minimum trading
-                days on your
-                evaluation. Unlocking opportunities and maximizing potential in the
-                dynamic world of trading.
-            </p>
+            <div className="relative hidden w-0 flex-1 lg:block bg-[#1e1e1e] pl-[120px] overflow-hidden">
+                <Badge className="mt-[91px] mb-[17px]">Start earning up to 90% profit</Badge>
+                <p className="text-stone-400 text-base font-normal leading-normal max-w-[434px]">
+                    No minimum trading days on your evaluation. Unlocking opportunities and maximizing potential in the
+                    dynamic world of trading.
+                </p>
 
-            <div
-                className="h-full w-full mt-[111px] overflow-hidden relative bg-[#151211] rounded-tl-[36px] shadow-[0px_30px_35px_32px_rgba(0,0,0,0.20)] border-l-8 border-t-8 border-[#474b54]"
-            >
-                <div className="mx-auto w-10/12 h-0">
-                    <Image
-                        src="/assets/images/img_3.png"
-                        alt="Trading Platform Interface"
-                        width={1000}
-                        height={700}
-                        layout="responsive"
-                        quality={100}
-                    />
+                <div
+                    className="h-full w-full mt-[111px] overflow-hidden relative bg-[#151211] rounded-tl-[36px] shadow-[0px_30px_35px_32px_rgba(0,0,0,0.20)] border-l-8 border-t-8 border-[#474b54]"
+                >
+                    <div className="mx-auto w-10/12 h-0">
+                        <Image
+                            src="/assets/images/img_3.png"
+                            alt="Trading Platform Interface"
+                            width={1000}
+                            height={700}
+                            layout="responsive"
+                            quality={100}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    );
 }
--- src/components/ui/input-text.tsx
+++ src/components/ui/input-text.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 interface InputTextProps extends React.InputHTMLAttributes<HTMLInputElement> {
     className?: string;
@@ -12,31 +12,45 @@ const InputText: React.FC<InputTextProps> = ({
                                                  placeholder = '',
                                                  name,
                                                  errorMessage = '',
+                                                 onChange,
                                                  ...props
                                              }) => {
+    const [internalValue, setInternalValue] = useState(props.value || '');
     const hasError = Boolean(errorMessage);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (onChange) {
+            onChange(event); // Si hay un manejador onChange definido, lo ejecuta
+        } else {
+            setInternalValue(event.target.value); // Si no, actualiza el estado interno
+        }
+    };
 
     return (
         <div className="w-full">
             <input
                 {...props}
+                value={onChange ? props.value : internalValue}
+                onChange={handleChange}
                 placeholder={placeholder}
                 name={name}
                 id={name}
                 aria-label={placeholder || name}
                 aria-invalid={hasError}
                 aria-describedby={hasError ? `${name}-error` : undefined}
-                className={`h-12 px-4 py-3 bg-[#1e1e1e]/70 rounded-xl border w-full focus:outline-none focus:ring-2 focus:ring-blue-500 ${
-                    hasError ? 'border-red-500' : 'border-neutral-700'
+                className={`h-12 px-4 py-3 bg-[#1e1e1e]/70 placeholder:text-neutral-700 rounded-xl border w-full focus:border-transparent focus:outline-none focus:ring-1 ${
+                    hasError
+                        ? 'ring-1 ring-red-500 text-red-500 border-transparent'
+                        : 'hover:border-white text-stone-400 border-neutral-700 focus:ring-mgt-primary'
                 } ${className}`}
             />
             {hasError && (
                 <span
                     id={`${name}-error`}
-                    className="text-sm text-red-500 mt-1 block"
+                    className="text-rose-500 text-xs mt-4 leading-tight"
                 >
-          {errorMessage}
-        </span>
+                    {errorMessage}
+                </span>
             )}
         </div>
     );
