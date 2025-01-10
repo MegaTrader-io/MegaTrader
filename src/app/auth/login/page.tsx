c56212dbb2a466ba5fa0842e335ac167f3fdbeee
Luis Viera <levieraf@gmail.com>
2025-01-09 22:34:02 -0500

    wip

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -186,7 +186,7 @@ export default function Login() {
                                         className="h-12 w-full px-4 py-3 bg-stone-800 rounded-xl border border-neutral-700 justify-center items-center gap-2 inline-flex"
                                     >
                                         <div className="text-neutral-50 text-base font-normal uppercase leading-normal">
-                                            Don’t have an account?
+                                            Create account
                                         </div>
                                     </Link>
                                 </div>
