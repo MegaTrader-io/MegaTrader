a1c63199d7e5c846e6934a2c346f0ce25393060d
Luis Viera <levieraf@gmail.com>
2025-01-11 18:14:21 -0500

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from "next";
 import {Space_Grotesk} from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 import {roboto} from '@/app/fonts/roboto'
 
 export const metadata: Metadata = {
@@ -17,11 +18,30 @@ const spaceGrotesk = Space_Grotesk({
     variable: "--font-space-grotesk"
 });
 
+const isProduction = process.env.NEXT_PUBLIC_ENVIRONMENT === "production";
+
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en" className={`${spaceGrotesk.variable} ${roboto.variable} dark`}>
         <body className="font-sans antialiased">
         {children}
+        {isProduction && (
+            <>
+                <Script id="livechat-script" strategy="lazyOnload">
+                    {`
+            window.__lc = window.__lc || {};
+            window.__lc.license = 18972174;
+            window.__lc.integration_name = "manual_channels";
+            window.__lc.product_name = "livechat";
+            ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
+            `}
+                </Script>
+                <noscript><a href="https://www.livechat.com/chat-with/18972174/" rel="nofollow">Chat with us</a>,
+                    powered by <a
+                        href="https://www.livechat.com/?welcome" rel="noopener nofollow" target="_blank">LiveChat</a>
+                </noscript>
+            </>
+        )}
         </body>
         </html>
     );
