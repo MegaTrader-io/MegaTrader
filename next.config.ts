0953743758a45aea602dc275315a917adb4cc933
Luis Viera <levieraf@gmail.com>
2025-01-11 18:22:49 -0500

    wip

--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,19 @@ const nextConfig: NextConfig = {
                 headers: [
                     {
                         key: 'Content-Security-Policy',
-                        value: `script-src 'self' 'unsafe-inline' https://cdn.livechatinc.com; object-src 'none'; frame-ancestors 'self';`,
+                        value: `script-src 'self' 'unsafe-inline' https://cdn.livechatinc.com https://api.livechatinc.com; object-src 'none'; frame-ancestors 'self'; connect-src 'self' https://api.livechatinc.com;`,
+                    },
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+                    },
+                    {
+                        key: 'Pragma',
+                        value: 'no-cache',
+                    },
+                    {
+                        key: 'Expires',
+                        value: '0',
                     },
                 ],
             },
