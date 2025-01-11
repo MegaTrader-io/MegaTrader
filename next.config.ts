327cf29781a8cdbd5a5864a8afcf75a41d311b1d
Luis Viera <levieraf@gmail.com>
2025-01-11 17:26:49 -0500

    wip

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type {NextConfig} from "next";
+import {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    basePath: '',
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'megatrader.io',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
