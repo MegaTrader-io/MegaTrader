3f2590b5ab4c49e30e2908e44707b8a40e543af5
Luis Viera <levieraf@gmail.com>
2025-01-12 22:46:17 -0500

    wijp

--- src/app/reset-password/change/page.tsx
+++ src/app/reset-password/change/page.tsx
@@ -1,51 +1,51 @@
 'use client';
 
 import InputText from "@/components/ui/input-text";
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import Badge from "@/components/ui/badge";
 import Image from "next/image";
-import React, {Suspense, useState} from "react";
-import {ChevronLeftIcon} from "@heroicons/react/16/solid";
+import React, { useState, useEffect } from "react";
+import { ChevronLeftIcon } from "@heroicons/react/16/solid";
 import Alert from "@/components/ui/Alert";
-import {useSearchParams} from "next/navigation";
 
 const TARGET_EMAIL = "test@megatrader.com";
 export const dynamic = 'force-dynamic';
 
 export default function ChangePassword() {
     const router = useRouter();
-    const searchParams = useSearchParams();
-    const email = searchParams.get("email") || "";
     const [form, setForm] = useState({
-        email,
-        password: '',
-        confirm_password: '',
+        email: "",
+        password: "",
+        confirm_password: "",
     });
     const [fieldErrors, setFieldErrors] = useState<Record<string, string>>({});
     const [isSubmitting, setIsSubmitting] = useState(false);
+
+    useEffect(() => {
+        const params = new URLSearchParams(window.location.search);
+        const email = params.get("email") || "";
+        setForm((prev) => ({ ...prev, email }));
+    }, []);
 
     function submitForm(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
 
         if (!form.password.trim()) {
-            setFieldErrors((prev) => ({...prev, password: "Ups... The email is not correct\""}));
+            setFieldErrors((prev) => ({ ...prev, password: "Password is required." }));
             return;
         }
 
         if (!form.confirm_password.trim()) {
-            setFieldErrors((prev) => ({
-                ...prev, confirm_password: "Ups... The email is not correct\""
-            }));
+            setFieldErrors((prev) => ({ ...prev, confirm_password: "Confirmation password is required." }));
             return;
         }
 
         if (form.password !== form.confirm_password) {
-            setFieldErrors(() => ({
+            setFieldErrors({
                 password: "The passwords do not match.",
-                confirm_password: "The passwords do not match."
-            }));
-
+                confirm_password: "The passwords do not match.",
+            });
             return;
         }
 
@@ -55,9 +55,11 @@ export default function ChangePassword() {
         setTimeout(() => {
             setIsSubmitting(false);
 
-            if (email === TARGET_EMAIL) {
-                router.push(`/auth/login?success-message=Your password have been changed successfully. Login in to your account.`);
-                return
+            if (form.email === TARGET_EMAIL) {
+                router.push(
+                    `/auth/login?success-message=Your password has been changed successfully. Log in to your account.`
+                );
+                return;
             }
 
             setFieldErrors({
@@ -67,110 +69,97 @@ export default function ChangePassword() {
     }
 
     return (
-        <Suspense>
-            <div className="flex flex-1">
-                <div
-                    className="flex w-full lg:w-2/5 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none  xl:px-24">
-                    <div className="mx-auto w-full max-w-[409px]">
-                        <div className="w-full mx-auto space-y-8">
-                            {fieldErrors.form && (
-                                <Alert type="error" message={fieldErrors.form}/>
-                            )}
+        <div className="flex flex-1">
+            <div className="flex w-full lg:w-2/5 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none xl:px-24">
+                <div className="mx-auto w-full max-w-[409px]">
+                    <div className="w-full mx-auto space-y-8">
+                        {fieldErrors.form && <Alert type="error" message={fieldErrors.form} />}
 
+                        <div>
+                            <h1 className="text-white text-5xl font-light uppercase leading-[60px] mb-2">
+                                Reset password
+                            </h1>
+                            <h2 className="text-stone-400 text-base font-normal font-roboto leading-normal tracking-wide">
+                                Enter and confirm your new password.
+                            </h2>
+                        </div>
+
+                        <form onSubmit={submitForm} className="space-y-4 lg:my-8">
                             <div>
-                                <h1 className="text-white text-nowrap text-5xl font-light uppercase leading-[60px] mb-2">
-                                    Reset password
-                                </h1>
-                                <h2
-                                    className="text-stone-400 text-base font-normal font-roboto leading-normal tracking-wide"
-                                >
-                                    Enter and confirm your new password.
-                                </h2>
+                                <InputText
+                                    type="password"
+                                    placeholder="Password"
+                                    name="password"
+                                    value={form.password}
+                                    onChange={(e) => {
+                                        const value = e.target.value;
+                                        setForm((prev) => ({ ...prev, [e.target.name]: value }));
+                                    }}
+                                    errorMessage={fieldErrors.password}
+                                />
                             </div>
 
-                            <form onSubmit={submitForm} className="space-y-4 lg:my-8">
-                                <div>
-                                    <InputText
-                                        type="password"
-                                        placeholder="Password"
-                                        name="password"
-                                        value={form.password}
-                                        onChange={(e) => {
-                                            const value = e.target.value;
-                                            setForm(prev => ({...prev, [e.target.name]: value}));
-                                        }}
-                                        errorMessage={fieldErrors.password}
-                                    />
+                            <div>
+                                <InputText
+                                    type="password"
+                                    placeholder="Confirm Password"
+                                    name="confirm_password"
+                                    value={form.confirm_password}
+                                    onChange={(e) => {
+                                        const value = e.target.value;
+                                        setForm((prev) => ({ ...prev, [e.target.name]: value }));
+                                    }}
+                                    errorMessage={fieldErrors.confirm_password}
+                                />
+                            </div>
+
+                            <button
+                                type="submit"
+                                disabled={isSubmitting}
+                                className={`h-12 w-full px-4 disabled:opacity-30 py-3 rounded-xl border-2 justify-center items-center gap-2 inline-flex ${
+                                    isSubmitting
+                                        ? "bg-gray-500 border-gray-500 cursor-not-allowed"
+                                        : "bg-mgt-primary border-mgt-primary"
+                                }`}
+                            >
+                                <div className="text-slate-950 text-base font-normal uppercase leading-normal">
+                                    {isSubmitting ? "Loading..." : "Update"}
                                 </div>
+                            </button>
 
-
-                                <div>
-                                    <InputText
-                                        type="password"
-                                        placeholder="Confirm Password"
-                                        name="confirm_password"
-                                        value={form.confirm_password}
-                                        onChange={(e) => {
-                                            const value = e.target.value;
-                                            setForm(prev => ({...prev, [e.target.name]: value}));
-                                        }}
-                                        errorMessage={fieldErrors.confirm_password}
-                                    />
+                            <Link
+                                href="/auth/login"
+                                className="h-12 w-full px-4 py-3 justify-center items-center gap-2 inline-flex"
+                            >
+                                <div className="text-neutral-50 text-base font-normal uppercase leading-normal flex">
+                                    <ChevronLeftIcon className="w-6 h-6 text-white" /> Return to login
                                 </div>
-
-                                <button
-                                    type="submit"
-                                    disabled={isSubmitting}
-                                    className={`h-12 w-full px-4 disabled:opacity-30 py-3 rounded-xl border-2 justify-center items-center gap-2 inline-flex ${
-                                        isSubmitting
-                                            ? "bg-gray-500 border-gray-500 cursor-not-allowed"
-                                            : "bg-mgt-primary border-mgt-primary"
-                                    }`}
-                                >
-                                    <div
-                                        className="text-slate-950 text-base font-normal uppercase leading-normal"
-                                    >
-                                        {isSubmitting ? "Loading..." : "Update"}
-                                    </div>
-                                </button>
-
-                                <Link
-                                    href="/auth/login"
-                                    className="h-12 w-full px-4 py-3 justify-center items-center gap-2 inline-flex"
-                                >
-                                    <div
-                                        className="text-neutral-50 text-base font-normal uppercase leading-normal flex">
-                                        <ChevronLeftIcon className="w-6 h-6 text-white"/> Return to login
-                                    </div>
-                                </Link>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                <div className="relative hidden w-0 flex-1 lg:block bg-[#1e1e1e] pl-[120px] overflow-hidden">
-                    <Badge className="mt-[91px] mb-[17px]">Start earning up to 90% profit</Badge>
-                    <p className="text-stone-400 text-base font-normal leading-normal max-w-[434px]">
-                        No minimum trading days on your evaluation. Unlocking opportunities and maximizing potential in
-                        the
-                        dynamic world of trading.
-                    </p>
-
-                    <div
-                        className="h-full w-full mt-[111px] overflow-hidden relative bg-[#151211] rounded-tl-[36px] shadow-[0px_30px_35px_32px_rgba(0,0,0,0.20)] border-l-8 border-t-8 border-[#474b54]"
-                    >
-                        <div className="mx-auto w-10/12 h-0">
-                            <Image
-                                src="/assets/images/img_3.png"
-                                alt="Trading Platform Interface"
-                                width={1000}
-                                height={700}
-                                layout="responsive"
-                                quality={100}
-                            />
-                        </div>
+                            </Link>
+                        </form>
                     </div>
                 </div>
             </div>
-        </Suspense>
+            <div className="relative hidden w-0 flex-1 lg:block bg-[#1e1e1e] pl-[120px] overflow-hidden">
+                <Badge className="mt-[91px] mb-[17px]">Start earning up to 90% profit</Badge>
+                <p className="text-stone-400 text-base font-normal leading-normal max-w-[434px]">
+                    No minimum trading days on your evaluation. Unlocking opportunities and maximizing potential in the
+                    dynamic world of trading.
+                </p>
+                <div
+                    className="h-full w-full mt-[111px] overflow-hidden relative bg-[#151211] rounded-tl-[36px] shadow-[0px_30px_35px_32px_rgba(0,0,0,0.20)] border-l-8 border-t-8 border-[#474b54]"
+                >
+                    <div className="mx-auto w-10/12 h-0">
+                        <Image
+                            src="/assets/images/img_3.png"
+                            alt="Trading Platform Interface"
+                            width={1000}
+                            height={700}
+                            layout="responsive"
+                            quality={100}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
