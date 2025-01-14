68126b02540a94c55c602a02e47395f1a6b87516
Luis Viera <levieraf@gmail.com>
2025-01-14 14:36:09 -0500

    wip

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,90 +2,26 @@ import Image from 'next/image';
 import Link from "next/link";
 import {useEffect, useState} from "react";
 import {Bars3Icon, BellIcon, UserCircleIcon} from "@heroicons/react/24/solid";
+import {usePathname} from "next/navigation";
 
 const navigationItems = [
-    {href: '/dashboard', label: 'ACCOUNT OVERVIEW', sectionId: 'dashboard'},
-    {href: '/affiliates', label: 'AFFILIATES', sectionId: 'affiliates'},
-    {href: '/payouts', label: 'PAYOUTS', sectionId: 'payouts'},
-    {href: '/help-center', label: 'HELP CENTER', sectionId: 'help-center'},
+    {href: '/account-overview', label: 'ACCOUNT OVERVIEW', sectionId: '/account-overview'},
+    {href: '/affiliates', label: 'AFFILIATES', sectionId: '/affiliates'},
+    {href: '/payouts', label: 'PAYOUTS', sectionId: '/payouts'},
+    {href: '/help-center', label: 'HELP CENTER', sectionId: '/help-center'},
 ];
 
 export default function Header() {
-    const [activeSection, setActiveSection] = useState('home');
-    const [hasScrolled, setHasScrolled] = useState(false);
+    const currentPath = usePathname()
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    useEffect(() => {
-        console.info('hasScrolled', hasScrolled);
-        const handleScroll = () => {
-            if (window.scrollY > 10) {
-                setHasScrolled(true);
-            } else {
-                setHasScrolled(false);
-            }
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
-
-    useEffect(() => {
-        const options = {
-            root: null,
-            rootMargin: '0px',
-            threshold: 0.6,
-        };
-
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    setActiveSection(entry.target.id);
-                }
-            });
-        }, options);
-
-        navigationItems.forEach(({sectionId}) => {
-            const element = document.getElementById(sectionId);
-            if (element) observer.observe(element);
-        });
-
-        return () => {
-            navigationItems.forEach(({sectionId}) => {
-                const element = document.getElementById(sectionId);
-                if (element) observer.unobserve(element);
-            });
-        };
-    }, []);
-
-    const handleClick = (e: React.MouseEvent<HTMLAnchorElement>, href: string) => {
-        e.preventDefault();
-        const element = document.querySelector(href);
-        if (element) {
-            const headerOffset = isMenuOpen ? 96 : 131;
-            const elementPosition = element.getBoundingClientRect().top + window.scrollY;
-            const offsetPosition = elementPosition - headerOffset;
-
-            window.scrollTo({
-                top: offsetPosition,
-                behavior: 'smooth',
-            });
-            setIsMenuOpen(false);
-        }
-    };
-
-    // return <header className={clsx('mx-auto w-full max-w-screen-xl bg-red-500 h-[100px] flex items-center', className)}>
-    //
-    // </header>
-
     return <div
-        className={`w-full z-50 transition-all duration-300 ${isMenuOpen ? 'bg-[#131210]' : 'bg-[#111]/80'}  backdrop-blur-3xl shadow-lg`}
-    >
+        className={`w-full z-50 transition-all duration-300 ${isMenuOpen ? 'bg-[#131210]' : 'bg-[#111]/80'}  backdrop-blur-3xl shadow-lg`}>
         <div className="w-full max-w-7xl mx-auto px-4 py-6 flex items-center justify-between lg:h-[100px]">
             {/* Logo */}
             <div className="w-auto">
                 <Link
-                    href="/"
-                    onClick={(e) => handleClick(e, '#home')}
+                    href="/account-overview"
                 >
                     <Image
                         src="../assets/images/megatrader-original.svg"
@@ -113,9 +49,8 @@ export default function Header() {
                     <Link
                         key={item.label}
                         href={item.href}
-                        onClick={(e) => handleClick(e, item.href)}
                         className={`text-base text-neutral-50 font-light uppercase leading-6 px-4 py-3 transition-all duration-200 ${
-                            activeSection === item.sectionId
+                            currentPath === item.sectionId
                                 ? 'text-white bg-[#1e1e1e] rounded-lg'
                                 : 'text-gray-400 hover:text-white'
                         }`}
@@ -134,9 +69,8 @@ export default function Header() {
                     <Link
                         key={item.label}
                         href={item.href}
-                        onClick={(e) => handleClick(e, item.href)}
                         className={`text-base text-neutral-50 text-nowrap font-light uppercase leading-6 px-4 py-3 transition-all duration-200 ${
-                            activeSection === item.sectionId
+                            currentPath === item.sectionId
                                 ? 'text-white bg-[#1e1e1e] rounded-lg'
                                 : 'text-gray-400 hover:text-white'
                         }`}
@@ -164,29 +98,18 @@ export default function Header() {
                         href="/auth/login"
                         className="bg-[#292524] rounded-xl border border-neutral-700 w-12 h-12 text-white uppercase text-nowrap flex items-center justify-center"
                     >
-                        <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <mask id="path-1-inside-1_4521_998" fill="white">
-                                <path
-                                    d="M0 12C0 5.37258 5.37258 0 12 0H36C42.6274 0 48 5.37258 48 12V36C48 42.6274 42.6274 48 36 48H12C5.37258 48 0 42.6274 0 36V12Z"/>
-                            </mask>
-                            <path
-                                d="M0 12C0 5.37258 5.37258 0 12 0H36C42.6274 0 48 5.37258 48 12V36C48 42.6274 42.6274 48 36 48H12C5.37258 48 0 42.6274 0 36V12Z"
-                                fill="#292524"/>
-                            <path
-                                d="M12 1H36V-1H12V1ZM47 12V36H49V12H47ZM36 47H12V49H36V47ZM1 36V12H-1V36H1ZM12 47C5.92487 47 1 42.0751 1 36H-1C-1 43.1797 4.8203 49 12 49V47ZM47 36C47 42.0751 42.0751 47 36 47V49C43.1797 49 49 43.1797 49 36H47ZM36 1C42.0751 1 47 5.92487 47 12H49C49 4.8203 43.1797 -1 36 -1V1ZM12 -1C4.8203 -1 -1 4.8203 -1 12H1C1 5.92487 5.92487 1 12 1V-1Z"
-                                fill="#404040" mask="url(#path-1-inside-1_4521_998)"/>
-                            <mask id="mask0_4521_998" style={{maskType: 'alpha'}} maskUnits="userSpaceOnUse" x="12"
-                                  y="12"
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <mask id="mask0_4562_2177" style={{maskType: 'alpha'}} maskUnits="userSpaceOnUse" x="0"
+                                  y="0"
                                   width="24" height="24">
-                                <rect x="12" y="12" width="24" height="24" fill="#D9D9D9"/>
+                                <rect width="24" height="24" fill="#D9D9D9"/>
                             </mask>
-                            <g mask="url(#mask0_4521_998)">
+                            <g mask="url(#mask0_4562_2177)">
                                 <path
-                                    d="M17 33C16.45 33 15.9792 32.8042 15.5875 32.4125C15.1958 32.0208 15 31.55 15 31V17C15 16.45 15.1958 15.9792 15.5875 15.5875C15.9792 15.1958 16.45 15 17 15H24V17H17V31H24V33H17ZM28 29L26.625 27.55L29.175 25H21V23H29.175L26.625 20.45L28 19L33 24L28 29Z"
+                                    d="M5 21C4.45 21 3.97917 20.8042 3.5875 20.4125C3.19583 20.0208 3 19.55 3 19V5C3 4.45 3.19583 3.97917 3.5875 3.5875C3.97917 3.19583 4.45 3 5 3H12V5H5V19H12V21H5ZM16 17L14.625 15.55L17.175 13H9V11H17.175L14.625 8.45L16 7L21 12L16 17Z"
                                     fill="white"/>
                             </g>
                         </svg>
-
                     </Link>
                 </div>
             </div>
