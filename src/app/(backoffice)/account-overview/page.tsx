215a8833808d32f50418f97d89436bccc267a877
Luis Viera <levieraf@gmail.com>
2025-01-15 11:16:00 -0500

    wip

--- src/app/(backoffice)/account-overview/page.tsx
+++ src/app/(backoffice)/account-overview/page.tsx
@@ -1,14 +1,53 @@
+'use client';
+
 import Card from "@/components/Card";
 import {Button} from "@/components/Button";
-import React from "react";
+import React, {useState} from "react";
 import Link from "next/link";
 import {PlusIcon} from "@heroicons/react/16/solid";
 import Dropdown from "@/components/Dropdown";
+import BaseBadge from "@/components/BaseBadge";
+import Image from "next/image";
+
+interface Account {
+    id: number
+    name: string
+    active: boolean
+}
+
+const accounts: Account[] = [
+    {id: 1, name: 'S1Sep2586479132', active: true},
+    {id: 2, name: 'S1Sep2586479133', active: false},
+];
 
 export default function AccountOverView() {
+    const [selectedAccount, setSelectedAccount] = useState(accounts[0]);
+
     return <>
         <Card className="w-full grid grid-cols-[auto_1fr_auto] items-center justify-between gap-4">
-            <Dropdown />
+            <Dropdown
+                items={accounts}
+                value={selectedAccount}
+                onChange={setSelectedAccount}
+                renderButtonContent={(item) => (
+                    <div className="flex gap-2 items-center">
+                        <BaseBadge shape="rounded"
+                                   variant={item.active ? 'secondary' : 'error'}
+                                   size="md">{item.active ? 'ACTIVE' : 'INACTIVE'}
+                        </BaseBadge>
+                        <div className="text-stone-400 text-base font-normal truncate">{item.name}</div>
+                        <Image src="/assets/images/arrow-down.svg" alt='selection' width={24} height={24}/>
+                    </div>
+                )}
+                renderOptionContent={(item) => (
+                    <>
+                        <BaseBadge shape="rounded" variant={item.active ? 'secondary' : 'error'}
+                                   size="md">{item.active ? 'ACTIVE' : 'INACTIVE'}</BaseBadge>
+                        <div className="text-stone-400 text-base font-normal truncate">{item.name}</div>
+                    </>
+                )}
+            />
+
             <div className="w-full">
                 <Link href="#" className="text-base btn-link">
                     Manage Subscription
--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,45 +1,42 @@
-'use client'
+import {Listbox, ListboxButton, ListboxOption, ListboxOptions} from '@headlessui/react';
+import clsx from 'clsx';
+import React, {useState} from 'react';
 
-import {Listbox, ListboxButton, ListboxOption, ListboxOptions} from '@headlessui/react'
-import clsx from 'clsx'
-import React, {useState} from 'react'
-import BaseBadge from "@/components/BaseBadge";
+interface DropdownProps<T> {
+    items: T[];
+    value: T;
+    onChange: (item: T) => void;
+    disabled?: boolean;
+    renderButtonContent: (item: T) => React.ReactNode;
+    renderOptionContent: (item: T) => React.ReactNode;
+}
 
-const accounts = [
-    {id: 1, name: 'S1Sep2586479132', active: true},
-    {id: 2, name: 'S1Sep25864791323', active: false},
-]
+export default function Dropdown<T extends { id: string | number }>({
+                                                                        items = [],
+                                                                        value,
+                                                                        onChange,
+                                                                        disabled = false,
+                                                                        renderButtonContent,
+                                                                        renderOptionContent,
+                                                                    }: DropdownProps<T>) {
+    const [selected, setSelected] = useState(value);
 
-export default function Example() {
-    const [selected, setSelected] = useState(accounts[1])
+    const handleChange = (item: T) => {
+        setSelected(item);
+        if (onChange) onChange(item);
+    };
 
     return (
         <div className="h-12 w-[256px]">
-            <Listbox disabled={false} value={selected} onChange={setSelected}>
+            <Listbox disabled={disabled} value={selected} onChange={handleChange}>
                 <ListboxButton
                     className={clsx(
                         'relative block w-full pl-4 pr-3 py-3 text-base font-normal leading-normal rounded-xl bg-stone-800 hover:bg-[#1e1e1e] border border-neutral-700 hover:border-transparent text-white  disabled:bg-stone-600 disabled:text-stone-800',
                         'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
                     )}
                 >
-                    <div className="flex gap-2 items-center">
-                        <BaseBadge shape="rounded"
-                                   variant={selected.active ? 'secondary' : 'error'}
-                                   size="sm">{selected.active ? 'ACTIVE' : 'INACTIVE'}
-                        </BaseBadge>
-                        <div className="text-stone-400 text-base font-normal truncate">{selected.name}</div>
-                        <div>
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <mask id="mask0_4588_2115" style={{maskType: 'alpha'}} maskUnits="userSpaceOnUse" x="0"
-                                      y="0" width="24" height="24">
-                                    <rect width="24" height="24" fill="#D9D9D9"/>
-                                </mask>
-                                <g mask="url(#mask0_4588_2115)">
-                                    <path d="M12 15L7 10H17L12 15Z" fill="white"/>
-                                </g>
-                            </svg>
-                        </div>
+                    <div className="flex gap-2 items-center uppercase">
+                        {renderButtonContent(selected)}
                     </div>
                 </ListboxButton>
                 <ListboxOptions
@@ -50,21 +47,17 @@ export default function Example() {
                         'transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0'
                     )}
                 >
-                    {accounts.map((account) => (
+                    {items.map((item) => (
                         <ListboxOption
-                            key={account.name}
-                            value={account}
+                            key={item.id}
+                            value={item}
                             className="group flex cursor-default items-center gap-2 rounded-lg px-4 py-3 select-none data-[focus]:bg-white/10"
                         >
-                            <BaseBadge shape="rounded"
-                                       variant={account.active ? 'secondary' : 'error'}
-                                       size="sm">{account.active ? 'ACTIVE' : 'INACTIVE'}
-                            </BaseBadge>
-                            <div className="text-stone-400 text-base font-normal truncate">{account.name}</div>
+                            {renderOptionContent(item)}
                         </ListboxOption>
                     ))}
                 </ListboxOptions>
             </Listbox>
         </div>
-    )
+    );
 }
