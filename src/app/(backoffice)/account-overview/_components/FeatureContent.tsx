2c77b125036a2e811c5482c73ad972b3c1fc7594
Luis Viera <levieraf@gmail.com>
2025-01-17 21:27:02 -0500

    wip

--- src/app/(backoffice)/account-overview/_components/FeatureContent.tsx
+++ src/app/(backoffice)/account-overview/_components/FeatureContent.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Card from "@/components/Card";
-import Image from "next/image";
 import QuestionIcon from "@/components/QuestionIcon";
 
 function FeatureContent() {
