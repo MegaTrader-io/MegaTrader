c562b9658f138ad4f24262c45a598772c20c6bbc
Luis Viera <levieraf@gmail.com>
2025-01-20 23:20:49 -0500

    wip

--- src/app/(backoffice)/account-overview/_components/FeatureContent.tsx
+++ src/app/(backoffice)/account-overview/_components/FeatureContent.tsx
@@ -41,7 +41,25 @@ function FeatureContent() {
                             AVG. PROFITABILITY PER TRADE
                         </div>
 
-                        <AvgProfitabilityPerTableTooltip/>
+                        <GenerictExclamationTooltip>
+                            <>
+                                <div className="text-white text-xs font-bold leading-tight">Avg. Profitability per
+                                    trade
+                                </div>
+                                <p className="leading-tight">
+                                    Average Profitability Per Trade (APPT) is the average amount you can expect to win
+                                    or lose per trade
+                                    based on your Average Winning Trade, Average Losing Trade, and Winning Trade %.
+                                    Average profit or
+                                    loss includes all fees and commissions.
+                                </p>
+                                <p className="leading-tight">
+                                    <span className="text-white">Tip:</span> This is your average P&L per trade. Keep
+                                    this number
+                                    positive to stay profitable.
+                                </p>
+                            </>
+                        </GenerictExclamationTooltip>
 
                         <div className="flex items-center gap-1">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
@@ -157,18 +175,18 @@ function FeatureContent() {
                             REWARD-TO-RISK RATIO
                         </div>
 
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <mask id="mask0_4398_10484" style={{maskType: 'alpha'}} maskUnits="userSpaceOnUse" x="0"
-                                  y="0"
-                                  width="24" height="24">
-                                <rect width="24" height="24" fill="#D9D9D9"/>
-                            </mask>
-                            <g mask="url(#mask0_4398_10484)">
-                                <path
-                                    d="M11 17H13V11H11V17ZM12 9C12.2833 9 12.5208 8.90417 12.7125 8.7125C12.9042 8.52083 13 8.28333 13 8C13 7.71667 12.9042 7.47917 12.7125 7.2875C12.5208 7.09583 12.2833 7 12 7C11.7167 7 11.4792 7.09583 11.2875 7.2875C11.0958 7.47917 11 7.71667 11 8C11 8.28333 11.0958 8.52083 11.2875 8.7125C11.4792 8.90417 11.7167 9 12 9ZM12 22C10.6167 22 9.31667 21.7375 8.1 21.2125C6.88333 20.6875 5.825 19.975 4.925 19.075C4.025 18.175 3.3125 17.1167 2.7875 15.9C2.2625 14.6833 2 13.3833 2 12C2 10.6167 2.2625 9.31667 2.7875 8.1C3.3125 6.88333 4.025 5.825 4.925 4.925C5.825 4.025 6.88333 3.3125 8.1 2.7875C9.31667 2.2625 10.6167 2 12 2C13.3833 2 14.6833 2.2625 15.9 2.7875C17.1167 3.3125 18.175 4.025 19.075 4.925C19.975 5.825 20.6875 6.88333 21.2125 8.1C21.7375 9.31667 22 10.6167 22 12C22 13.3833 21.7375 14.6833 21.2125 15.9C20.6875 17.1167 19.975 18.175 19.075 19.075C18.175 19.975 17.1167 20.6875 15.9 21.2125C14.6833 21.7375 13.3833 22 12 22Z"
-                                    fill="white"/>
-                            </g>
-                        </svg>
+                        <GenerictExclamationTooltip>
+                            <>
+                                <div className="text-white text-xs font-bold leading-tight">Reward-to-risk ratio
+                                </div>
+                                <p className="leading-tight">
+                                    Mesures the potential reward (profit) you achieve per trade VS the risk (losses) you take.
+                                </p>
+                                <p className="leading-tight">
+                                    <span className="text-white">Tip:</span> One of the most important metrics to successful trading! Less risk and more reward increases your probability of continued profitability.
+                                </p>
+                            </>
+                        </GenerictExclamationTooltip>
 
                         <div className="flex items-center">
                             <div
@@ -218,20 +236,14 @@ function QuestionTooltip() {
     </Tooltip>
 }
 
-function AvgProfitabilityPerTableTooltip() {
+function GenerictExclamationTooltip({children}: { children?: React.ReactElement }) {
     return <Tooltip>
         <TooltipTrigger>
             <ExclamationIcon className="text-white"/>
         </TooltipTrigger>
         <TooltipContent className="p-3">
             <div className="w-[265px] text-stone-400 text-xs font-normal leading-tight space-y-2">
-                <div className="text-white text-xs font-bold leading-tight">Avg. Profitability per trade</div>
-                <p className="leading-tight">
-                    Average Profitability Per Trade (APPT) is the average amount you can expect to win or lose per trade based on your Average Winning Trade, Average Losing Trade, and Winning Trade %. Average profit or loss includes all fees and commissions.
-                </p>
-                <p className="leading-tight">
-                    <span className="text-white">Tip:</span> This is your average P&L per trade. Keep this number positive to stay profitable.
-                </p>
+                {children}
             </div>
         </TooltipContent>
     </Tooltip>
