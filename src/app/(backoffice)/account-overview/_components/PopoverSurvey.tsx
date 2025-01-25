c7ddf6daac37edbc7454aed5a2a682323db2359a
Luis Viera <levieraf@gmail.com>
2025-01-25 13:39:27 -0500

    wip

--- src/app/(backoffice)/account-overview/_components/PopoverSurvey.tsx
+++ src/app/(backoffice)/account-overview/_components/PopoverSurvey.tsx
@@ -146,25 +146,6 @@ function PopoverSurvey({surveyData, onClick}: {
                     {canEdit ? 'SAVE' : 'EDIT'}
                 </Button>
             </div>
-            <BasePopover.Arrow asChild>
-                <svg width="29" height="16" viewBox="0 1 29 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g filter="url(#filter0_d_4706_5257)">
-                        <path d="M14.5 14L28.5 0H0.5L14.5 14Z" fill="#131210"/>
-                    </g>
-                    <defs>
-                        <filter id="filter0_d_4706_5257" x="0.5" y="0" width="28" height="15.5" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-                            <feFlood floodOpacity="0" result="BackgroundImageFix"/>
-                            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                            <feOffset dy="1.6"/>
-                            <feComposite in2="hardAlpha" operator="out"/>
-                            <feColorMatrix type="matrix" values="0 0 0 0 0.25098 0 0 0 0 0.25098 0 0 0 0 0.25098 0 0 0 1 0"/>
-                            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_4706_5257"/>
-                            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_4706_5257" result="shape"/>
-                        </filter>
-                    </defs>
-                </svg>
-            </BasePopover.Arrow>
-
         </PopoverContent>
     );
 }
