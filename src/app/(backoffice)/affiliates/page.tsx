53fcdf7858720776cb77b3ed4ed25eadcb19661c
Luis Viera <levieraf@gmail.com>
2025-01-29 23:40:47 -0500

    wip

--- src/app/(backoffice)/affiliates/page.tsx
+++ src/app/(backoffice)/affiliates/page.tsx
@@ -3,8 +3,27 @@ import React from "react";
 
 export default function AccountOverView() {
     return <>
-        <Card className="w-full grid grid-cols-[256px_1fr_auto] items-center justify-between gap-4 text-white">
-            AFFILIATES
+        <div className="grid grid-cols-12 gap-4 w-full">
+            <Card className="col-span-7 w-full p-6 text-white">
+                EARN WITH MEGATRADER
+            </Card>
+            <Card className="col-span-5 w-full p-6 text-white">
+                INVITE YOUR FRIENDS
+            </Card>
+        </div>
+        <Card className="w-full p-4 text-white">
+            SUMMARY
         </Card>
+        <div className="grid grid-cols-3 gap-4 w-full h-32">
+            <Card className="w-full bg-primary p-4 text-[#131210]">
+
+            </Card>
+            <Card className="w-full p-4 text-white">
+
+            </Card>
+            <Card className="w-full p-4 text-white">
+
+            </Card>
+        </div>
     </>
 }
