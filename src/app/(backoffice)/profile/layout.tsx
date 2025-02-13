14dd2cafd53b3f4d7e6053704fc25005bf9c9e40
Luis Viera <levieraf@gmail.com>
2025-02-12 21:38:00 -0500

    wip

--- src/app/(backoffice)/profile/layout.tsx
+++ src/app/(backoffice)/profile/layout.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {usePathname, useRouter} from "next/navigation";
 import clsx from "clsx";
 import Link from "@/components/Link";
