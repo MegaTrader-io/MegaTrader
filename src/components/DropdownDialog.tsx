19fb5b0e4ab7c4bda8fa30582880594bcd8ac7d5
Luis Viera <levieraf@gmail.com>
2025-02-16 13:02:08 -0500

    wip

--- src/components/DropdownDialog.tsx
+++ src/components/DropdownDialog.tsx
@@ -44,7 +44,7 @@ export default function DropdownDialog<T extends { id: string | number, name: st
             <AlertDialogPortal>
                 <AlertDialogOverlay className="fixed inset-0 bg-[#131210]/60 data-[state=open]:animate-overlayShow"/>
                 <AlertDialogContent
-                    className="fixed left-1/2 top-1/2 max-h-[85vh] w-[328px] -translate-x-1/2 -translate-y-1/2 rounded-2xl p-4 bg-[#131210] shadow-[0px_20px_20px_20px_rgba(0,0,0,0.10)] border border-neutral-700 flex-col justify-start items-center gap-8 inline-flex overflow-hidden focus:outline-none data-[state=open]:animate-contentShow">
+                    className="fixed left-1/2 top-1/2 max-h-[85vh] w-[360px] -translate-x-1/2 -translate-y-1/2 rounded-2xl p-4 bg-[#131210] shadow-[0px_20px_20px_20px_rgba(0,0,0,0.10)] border border-neutral-700 flex-col justify-start items-center gap-8 inline-flex overflow-hidden focus:outline-none data-[state=open]:animate-contentShow">
                     <AlertDialogTitle className="w-full">
                         <div className="flex justify-between items-center w-full">
                             <div className="text-white text-2xl font-medium uppercase leading-7">
