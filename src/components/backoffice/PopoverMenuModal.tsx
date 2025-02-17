ff65af03c659afc58fb272c2150688608a12c618
Luis Viera <levieraf@gmail.com>
2025-02-17 13:57:17 -0500

    wo

--- src/components/backoffice/PopoverMenuModal.tsx
+++ src/components/backoffice/PopoverMenuModal.tsx
@@ -85,7 +85,7 @@ function PopoverMenu({className, children, icon, modal = true, side = "bottom", 
         <div className={className}>
             <Popover modal={!!(modal && isMobile)} open={open} onOpenChange={setOpen}>
                 <PopoverTrigger asChild>
-                    <button ref={buttonRef} className="btn-primary block w-12">
+                    <button ref={buttonRef} className="bg-[#292524] rounded-xl border border-neutral-700 p-3 w-12 h-12 items-center justify-center">
                         {!icon && <Bars3Icon className="w-6 h-6 text-white"/>}
                         {icon && icon}
                     </button>
