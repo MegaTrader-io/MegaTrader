7d62e8e1db4cb17f7b628e1f4cd478bd954a65b8
Luis Viera <levieraf@gmail.com>
2025-02-23 02:23:07 -0500

    wip

---
+++ src/app/(backoffice)/account-overview/_components/DailyLossLimit.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {formatCurrency} from "@/commons/utils";
+import ExclamationTooltip from "@/app/(backoffice)/account-overview/_components/ExclamationTooltip";
+
+function DailyLossLimit({dailyLossLimit}: { dailyLossLimit: number }): React.ReactNode {
+    return <div className="flex items-center gap-2">
+        {formatCurrency(dailyLossLimit, 0)}
+        <ExclamationTooltip
+            className='text-stone-400'
+            title={'Realised P&L amount at any time during the trading week (Sunday 5:00 PM - Friday 3:10 PM CT)'}/>
+    </div>
+}
+
+export default DailyLossLimit;
