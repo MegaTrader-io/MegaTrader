9839a89d0421ecd739f0c495d9411e42511d3f44
Luis Viera <levieraf@gmail.com>
2025-03-05 08:38:58 -0500

    wip

--- src/app/(backoffice)/profile/personal-information/page.tsx
+++ src/app/(backoffice)/profile/personal-information/page.tsx
@@ -55,162 +55,168 @@ function Page() {
     }
 
     return (
-        <Card className="w-full space-y-4">
+        <>
             {hasErrors &&
                 <Alert className="w-full" type="error" message='Somethig went wrogn. Please try again later.'/>}
             {updated &&
                 <Alert className="w-full" type="success"
                        message='Great! Your personal information have been updated successfully'/>}
-            <form onSubmit={onSubmit} className="w-full text-white md:grid md:grid-cols-2 gap-4">
-                <div className="space-y-4">
-                    <div>
-                        <label className="text-stone-400 text-base font-bold leading-normal w-full">
-                            Address
-                            <InputText name={'address'} value={user.address} onChange={changeFields}
-                                       errorMessage={errors.address}/>
-                        </label>
-                    </div>
-                    <div>
-                        <label className="text-stone-400 text-base font-bold leading-normal w-full">
-                            Zip-code
-                            <InputText name={'zipCode'} value={user.zipCode} onChange={changeFields}
-                                       errorMessage={errors.zipCode}/>
-                        </label>
-                    </div>
-                    <div>
-                        <label className="text-stone-400 text-base font-bold leading-normal">
-                            Country
+            <Card className="w-full space-y-4">
+                <form onSubmit={onSubmit} className="w-full text-white md:grid md:grid-cols-2 gap-4">
+                    <div className="space-y-4">
+                        <div>
+                            <label className="text-stone-400 text-base font-bold leading-normal w-full">
+                                Address
+                                <InputText name={'address'} value={user.address} onChange={changeFields}
+                                           errorMessage={errors.address}/>
+                            </label>
+                        </div>
+                        <div>
+                            <label className="text-stone-400 text-base font-bold leading-normal w-full">
+                                Zip-code
+                                <InputText name={'zipCode'} value={user.zipCode} onChange={changeFields}
+                                           errorMessage={errors.zipCode}/>
+                            </label>
+                        </div>
+                        <div>
+                            <label className="text-stone-400 text-base font-bold leading-normal">
+                                Country
 
-                            <div>
-                                <div className="relative w-full">
-                                    <select
-                                        name="country"
-                                        value={user.country}
-                                        className={clsx('w-full py-3 px-4 pr-10 rounded-xl border border-neutral-700 text-stone-400 bg-[#1e1e1e]/70 appearance-none focus:outline-none',
-                                            errors.country
-                                                ? 'ring-1 ring-red-500 text-red-500 border-transparent'
-                                                : ''
-                                        )}
-                                        onChange={changeFields}
-                                    >
-                                        <option value=""></option>
-                                        {countries.map(country => (
-                                            <option key={country.id} value={country.id}>{country.description}</option>
-                                        ))}
-                                    </select>
-                                    <div className="absolute inset-y-0 right-3 flex items-center pointer-events-none">
-                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                             xmlns="http://www.w3.org/2000/svg">
-                                            <mask id="mask0_5269_2288" style={{maskType: 'alpha'}}
-                                                  maskUnits="userSpaceOnUse" x="0" y="0"
-                                                  width="24" height="24">
-                                                <rect width="24" height="24" fill="#D9D9D9"/>
-                                            </mask>
-                                            <g mask="url(#mask0_5269_2288)">
-                                                <path d="M12 15L7 10H17L12 15Z" fill="white"/>
-                                            </g>
-                                        </svg>
+                                <div>
+                                    <div className="relative w-full">
+                                        <select
+                                            name="country"
+                                            value={user.country}
+                                            className={clsx('w-full py-3 px-4 pr-10 rounded-xl border border-neutral-700 text-stone-400 bg-[#1e1e1e]/70 appearance-none focus:outline-none',
+                                                errors.country
+                                                    ? 'ring-1 ring-red-500 text-red-500 border-transparent'
+                                                    : ''
+                                            )}
+                                            onChange={changeFields}
+                                        >
+                                            <option value=""></option>
+                                            {countries.map(country => (
+                                                <option key={country.id}
+                                                        value={country.id}>{country.description}</option>
+                                            ))}
+                                        </select>
+                                        <div
+                                            className="absolute inset-y-0 right-3 flex items-center pointer-events-none">
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                                 xmlns="http://www.w3.org/2000/svg">
+                                                <mask id="mask0_5269_2288" style={{maskType: 'alpha'}}
+                                                      maskUnits="userSpaceOnUse" x="0" y="0"
+                                                      width="24" height="24">
+                                                    <rect width="24" height="24" fill="#D9D9D9"/>
+                                                </mask>
+                                                <g mask="url(#mask0_5269_2288)">
+                                                    <path d="M12 15L7 10H17L12 15Z" fill="white"/>
+                                                </g>
+                                            </svg>
+                                        </div>
                                     </div>
-                                </div>
-                                {errors.country && (
-                                    <span
-                                        id={`country-error`}
-                                        className="text-rose-500 text-xs mt-4 leading-tight"
-                                    >
+                                    {errors.country && (
+                                        <span
+                                            id={`country-error`}
+                                            className="text-rose-500 text-xs mt-4 leading-tight"
+                                        >
                     {errors.country}
                 </span>
-                                )}
-                            </div>
-                        </label>
-                    </div>
-                    <div>
-                        <label className="text-stone-400 text-base font-bold leading-normal">
-                            Language
-                            <div>
-                                <div className="relative w-full">
-                                    <select
-                                        value={user.language}
-                                        name="language"
-                                        className={clsx('w-full py-3 px-4 pr-10 rounded-xl border border-neutral-700 text-stone-400 bg-[#1e1e1e]/70 appearance-none focus:outline-none',
-                                            errors.language
-                                                ? 'ring-1 ring-red-500 text-red-500 border-transparent'
-                                                : ''
-                                        )}
-                                        onChange={changeFields}
-                                    >
-                                        <option value=""></option>
-                                        {languages.map(language => (
-                                            <option key={language.id}
-                                                    value={language.id}>{language.description}</option>
-                                        ))}
-                                    </select>
-                                    <div className="absolute inset-y-0 right-3 flex items-center pointer-events-none">
-                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                             xmlns="http://www.w3.org/2000/svg">
-                                            <mask id="mask0_5269_2288" style={{maskType: 'alpha'}}
-                                                  maskUnits="userSpaceOnUse" x="0" y="0"
-                                                  width="24" height="24">
-                                                <rect width="24" height="24" fill="#D9D9D9"/>
-                                            </mask>
-                                            <g mask="url(#mask0_5269_2288)">
-                                                <path d="M12 15L7 10H17L12 15Z" fill="white"/>
-                                            </g>
-                                        </svg>
-                                    </div>
+                                    )}
                                 </div>
+                            </label>
+                        </div>
+                        <div>
+                            <label className="text-stone-400 text-base font-bold leading-normal">
+                                Language
+                                <div>
+                                    <div className="relative w-full">
+                                        <select
+                                            value={user.language}
+                                            name="language"
+                                            className={clsx('w-full py-3 px-4 pr-10 rounded-xl border border-neutral-700 text-stone-400 bg-[#1e1e1e]/70 appearance-none focus:outline-none',
+                                                errors.language
+                                                    ? 'ring-1 ring-red-500 text-red-500 border-transparent'
+                                                    : ''
+                                            )}
+                                            onChange={changeFields}
+                                        >
+                                            <option value=""></option>
+                                            {languages.map(language => (
+                                                <option key={language.id}
+                                                        value={language.id}>{language.description}</option>
+                                            ))}
+                                        </select>
+                                        <div
+                                            className="absolute inset-y-0 right-3 flex items-center pointer-events-none">
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                                 xmlns="http://www.w3.org/2000/svg">
+                                                <mask id="mask0_5269_2288" style={{maskType: 'alpha'}}
+                                                      maskUnits="userSpaceOnUse" x="0" y="0"
+                                                      width="24" height="24">
+                                                    <rect width="24" height="24" fill="#D9D9D9"/>
+                                                </mask>
+                                                <g mask="url(#mask0_5269_2288)">
+                                                    <path d="M12 15L7 10H17L12 15Z" fill="white"/>
+                                                </g>
+                                            </svg>
+                                        </div>
+                                    </div>
 
-                                {errors.language && (
-                                    <span
-                                        id={`language-error`}
-                                        className="text-rose-500 text-xs mt-4 leading-tight"
-                                    >
+                                    {errors.language && (
+                                        <span
+                                            id={`language-error`}
+                                            className="text-rose-500 text-xs mt-4 leading-tight"
+                                        >
                     {errors.language}
                 </span>
-                                )}
+                                    )}
 
-                            </div>
-                        </label>
+                                </div>
+                            </label>
+                        </div>
                     </div>
-                </div>
-                <div className="space-y-4">
-                    <div>
-                        <label className="text-stone-400 text-base font-bold leading-normal">
-                            City
-                            <InputText name={'city'}
-                                       value={user.city}
-                                       onChange={changeFields}
-                                       errorMessage={errors.city}/>
-                        </label>
+                    <div className="space-y-4">
+                        <div>
+                            <label className="text-stone-400 text-base font-bold leading-normal">
+                                City
+                                <InputText name={'city'}
+                                           value={user.city}
+                                           onChange={changeFields}
+                                           errorMessage={errors.city}/>
+                            </label>
+                        </div>
+                        <div>
+                            <label className="text-stone-400 text-base font-bold leading-normal">
+                                State
+                                <InputText
+                                    name={'state'}
+                                    value={user.state}
+                                    onChange={changeFields}
+                                    errorMessage={errors.state}/>
+                            </label>
+                        </div>
+                        <div>
+                            <label className="text-stone-400 text-base font-bold leading-normal">
+                                Phone
+                                <InputText
+                                    name={'phone'}
+                                    value={user.phone}
+                                    onChange={changeFields}
+                                    errorMessage={errors.phone}/>
+                            </label>
+                        </div>
                     </div>
-                    <div>
-                        <label className="text-stone-400 text-base font-bold leading-normal">
-                            State
-                            <InputText
-                                name={'state'}
-                                value={user.state}
-                                onChange={changeFields}
-                                errorMessage={errors.state}/>
-                        </label>
-                    </div>
-                    <div>
-                        <label className="text-stone-400 text-base font-bold leading-normal">
-                            Phone
-                            <InputText
-                                name={'phone'}
-                                value={user.phone}
-                                onChange={changeFields}
-                                errorMessage={errors.phone}/>
-                        </label>
-                    </div>
-                </div>
 
-                <div className="mt-4 md:mt-0">
-                    <Button type='submit' variant={'primary'}>
-                        Save changes
-                    </Button>
-                </div>
-            </form>
-        </Card>
+                    <div className="mt-4 md:mt-0">
+                        <Button type='submit' variant={'primary'}>
+                            Save changes
+                        </Button>
+                    </div>
+                </form>
+            </Card>
+        </>
+
     );
 }
 
