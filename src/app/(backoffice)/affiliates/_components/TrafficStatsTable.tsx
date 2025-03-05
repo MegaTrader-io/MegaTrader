4b3674758651e80a9b67afbceac6e76ec045dd4c
Luis Viera <levieraf@gmail.com>
2025-03-04 20:15:42 -0500

    wip

--- src/app/(backoffice)/affiliates/_components/TrafficStatsTable.tsx
+++ src/app/(backoffice)/affiliates/_components/TrafficStatsTable.tsx
@@ -6,6 +6,7 @@ import {OptionInterface} from "@/commons/interfaces";
 import URLVisitsTable from "@/app/(backoffice)/affiliates/_components/URLVisitsTable";
 import PayoutsTable from "@/app/(backoffice)/affiliates/_components/PayoutsTable";
 import IncomeTable from "@/app/(backoffice)/affiliates/_components/IncomeTable";
+import {featureContentOptions} from "@/commons/data";
 
 const Options: OptionInterface[] = [
     {id: 'url_visits', label: 'URL Visits'},
@@ -31,20 +32,47 @@ function SelectionTab({onClick, selection}: { onClick: (option: OptionInterface)
 function TrafficStatsTable() {
     const [selection, setSelection] = useState(Options[0].id);
 
-    function changeSelection(option: OptionInterface) {
+    function clickOption(option: OptionInterface) {
         setSelection(option.id)
+    }
+
+    function changeOption(ev: React.ChangeEvent<HTMLSelectElement>) {
+        setSelection(ev.target.value)
     }
 
     return (
         <div className="w-full space-y-2 lg:space-y-4">
             <div className="hidden lg:flex gap-2">
-                <SelectionTab onClick={changeSelection} selection={selection}/>
+                <SelectionTab onClick={clickOption} selection={selection}/>
+            </div>
+
+            <div className="lg:hidden relative w-full">
+                <select
+                    value={selection}
+                    className="w-full py-3 px-4 pr-10 rounded-xl border border-neutral-700 text-stone-400 bg-[#1e1e1e]/70 appearance-none focus:outline-none"
+                    onChange={changeOption}>
+                    {Options.map(option => (
+                        <option key={option.id} value={option.id}>
+                            {option.label}
+                        </option>
+                    ))}
+                </select>
+                <div className="absolute inset-y-0 right-3 flex items-center pointer-events-none">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <mask id="mask0_5269_2288" style={{maskType: 'alpha'}} maskUnits="userSpaceOnUse" x="0"
+                              y="0"
+                              width="24" height="24">
+                            <rect width="24" height="24" fill="#D9D9D9"/>
+                        </mask>
+                        <g mask="url(#mask0_5269_2288)">
+                            <path d="M12 15L7 10H17L12 15Z" fill="white"/>
+                        </g>
+                    </svg>
+                </div>
             </div>
 
             <Card className="space-y-4">
-                <div className="flex lg:hidden gap-2">
-                    <SelectionTab onClick={changeSelection} selection={selection}/>
-                </div>
                 {selection === 'url_visits' && <URLVisitsTable/>}
                 {selection === 'payouts' && <PayoutsTable/>}
                 {selection === 'income' && <IncomeTable/>}
