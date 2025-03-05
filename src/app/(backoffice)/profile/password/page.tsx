0dfe48bee0ea95feaa3e585196d783a889544fbe
Luis Viera <levieraf@gmail.com>
2025-03-05 08:47:51 -0500

    wip

--- src/app/(backoffice)/profile/password/page.tsx
+++ src/app/(backoffice)/profile/password/page.tsx
@@ -4,6 +4,7 @@ import React, {useState} from 'react';
 import InputText from "@/components/InputText";
 import {Button} from "@/components/Button";
 import Alert from "@/components/Alert";
+import Card from "@/components/Card";
 
 function Page() {
     const [credentials, setCredentials] = useState({
@@ -57,56 +58,58 @@ function Page() {
     }
 
     return (
-        <div className="w-full space-y-4">
+        <>
             {hasErrors &&
                 <Alert className="w-full" type="error" message='Somethig went wrogn. Please try again later.'/>}
             {updated &&
                 <Alert className="w-full" type="success"
                        message='Great! Your personal information have been updated successfully'/>}
-            <form onSubmit={onSubmit} className="w-full text-white">
-                <div className="space-y-4">
-                    <div>
-                        <label className="text-stone-400 text-base font-bold leading-normal w-full">
-                            Current password
-                            <InputText
-                                type="password"
-                                name={'currentPassword'}
-                                value={credentials.currentPassword}
-                                onChange={changeFields}
-                                errorMessage={errors.currentPassword}/>
-                        </label>
-                    </div>
-                    <div>
-                        <label className="text-stone-400 text-base font-bold leading-normal w-full">
-                            New password
-                            <InputText
-                                type={'password'}
-                                name={'newPassword'}
-                                value={credentials.newPassword}
-                                onChange={changeFields}
-                                errorMessage={errors.newPassword}/>
-                        </label>
-                    </div>
-                    <div>
-                        <label className="text-stone-400 text-base font-bold leading-normal w-full">
-                            Confirm password
-                            <InputText
-                                type={'password'}
-                                name={'confirmPassword'}
-                                value={credentials.confirmPassword}
-                                onChange={changeFields}
-                                errorMessage={errors.confirmPassword}/>
-                        </label>
-                    </div>
+            <Card className="w-full space-y-4">
+                <form onSubmit={onSubmit} className="w-full text-white">
+                    <div className="space-y-4">
+                        <div>
+                            <label className="text-stone-400 text-base font-bold leading-normal w-full">
+                                Current password
+                                <InputText
+                                    type="password"
+                                    name={'currentPassword'}
+                                    value={credentials.currentPassword}
+                                    onChange={changeFields}
+                                    errorMessage={errors.currentPassword}/>
+                            </label>
+                        </div>
+                        <div>
+                            <label className="text-stone-400 text-base font-bold leading-normal w-full">
+                                New password
+                                <InputText
+                                    type={'password'}
+                                    name={'newPassword'}
+                                    value={credentials.newPassword}
+                                    onChange={changeFields}
+                                    errorMessage={errors.newPassword}/>
+                            </label>
+                        </div>
+                        <div>
+                            <label className="text-stone-400 text-base font-bold leading-normal w-full">
+                                Confirm password
+                                <InputText
+                                    type={'password'}
+                                    name={'confirmPassword'}
+                                    value={credentials.confirmPassword}
+                                    onChange={changeFields}
+                                    errorMessage={errors.confirmPassword}/>
+                            </label>
+                        </div>
 
-                    <div>
-                        <Button type='submit' variant={'primary'}>
-                            Save changes
-                        </Button>
+                        <div>
+                            <Button type='submit' variant={'primary'}>
+                                Save changes
+                            </Button>
+                        </div>
                     </div>
-                </div>
-            </form>
-        </div>
+                </form>
+            </Card>
+        </>
     );
 }
 
