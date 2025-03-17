057c38f97278e625e6da3acf9ff485c1cece045f
Luis Viera <levieraf@gmail.com>
2025-03-17 14:57:07 -0500

    wip

--- src/app/(backoffice)/account-overview/_components/AccountSummary.tsx
+++ src/app/(backoffice)/account-overview/_components/AccountSummary.tsx
@@ -17,8 +17,8 @@ function AccountSummary({account}: { account: Account }) {
     const {overallPerformance} = account;
 
     return (
-        <Card className="space-y-8 md:space-y-0 md:grid md:grid-cols-2 gap-8 w-full">
-            <div id="account-overview" className="space-y-4">
+        <Card className="space-y-8 md:space-y-0 md:grid md:grid-cols-2 gap-8 w-full px-0">
+            <div id="account-overview" className="space-y-4 px-4">
                 <div className="text-white text-xl font-light uppercase leading-normal">OVERALL PERFORMANCE</div>
                 <div className="gap-4 lg:flex lg:items-center">
                     <div className="lg:flex flex-col items-start relative flex-1 grow">
@@ -71,10 +71,10 @@ function AccountSummary({account}: { account: Account }) {
                 </div>
             </div>
             <div id="challenge-payout-objectives" className="space-y-4">
-                <div className="text-white text-xl font-light uppercase leading-normal">Your Challenge Objective</div>
+                <div className="text-white text-xl font-light uppercase leading-normal px-4">Your Challenge Objective</div>
                 <div className="gap-4 lg:flex lg:items-center">
                     <div className="lg:flex flex-col items-start relative flex-1 grow space-y-8">
-                        <div className="w-full">
+                        <div className="w-full px-4">
                             {account.objectives.profitTarget &&
                                 <ObjectiveItemMoney label={'Profit Target'}
                                                     objective={account.objectives.profitTarget}/>
@@ -98,10 +98,10 @@ function AccountSummary({account}: { account: Account }) {
                                 <HighestProfitDay account={account}/>}
                         </div>
                         <div id="rules-compliance" className="w-full">
-                            <div className="text-white text-xl font-light uppercase leading-normal mb-4">
+                            <div className="text-white text-xl font-light uppercase leading-normal mb-4 px-4">
                                 <span>RULE</span>
                             </div>
-                            <div className="flex gap-2">
+                            <div className="flex gap-2 px-4">
                                 <div className="flex items-center gap-2 h-full">
                                     <div className="h-full">
                                         <div className="flex items-center gap-2">
