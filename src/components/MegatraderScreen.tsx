a3acc841a80f5d5251b81c2e3472e83475274f53
Luis Viera <levieraf@gmail.com>
2025-04-07 10:53:20 -0500

    wip

--- src/components/MegatraderScreen.tsx
+++ src/components/MegatraderScreen.tsx
@@ -43,6 +43,8 @@ const MegatraderScreen = () => {
         dots: true,
         infinite: true,
         speed: 500,
+        autoplay: true,
+        autoplaySpeed: 3000,
         slidesToShow: 1,
         slidesToScroll: 1,
     };
