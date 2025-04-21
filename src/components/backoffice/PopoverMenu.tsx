f9553ee48ac32c6842dbaf534973ca61dbac4ae8
Luis Viera <levieraf@gmail.com>
2025-04-21 14:47:38 -0500

    wip

--- src/components/backoffice/PopoverMenu.tsx
+++ src/components/backoffice/PopoverMenu.tsx
@@ -58,6 +58,7 @@ function PopoverMenu({
     };
 
     const handleOpenMenu = (open: boolean) => {
+        const isMobile = window.innerWidth <= 768;
         const doesExistsIntroTooltip = document.querySelector('.introjs-tooltip');
         if (doesExistsIntroTooltip && isMobile) {
             return;
