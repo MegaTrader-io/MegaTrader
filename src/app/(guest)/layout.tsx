fe40b7dbf7b8aed512f682f3079861c310c6bf34
Luis Viera <levieraf@gmail.com>
2025-05-01 18:03:38 -0500

    wip

--- src/app/(guest)/layout.tsx
+++ src/app/(guest)/layout.tsx
@@ -3,18 +3,16 @@ import MegatraderScreen from "@/components/MegatraderScreen";
 
 const Layout = ({children}: { children: React.ReactNode }) => {
     return (
-        <div className="lg:h-dvh m-4 lg:flex lg:flex-1">
+        <div className="xl:h-dvh m-4 xl:flex xl:flex-1">
             <div
-                className="flex h-full md:h-auto w-full lg:w-2/5 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none xl:px-24">
+                className="flex h-full md:h-auto w-full xl:w-2/5 flex-col justify-center px-4 py-12 sm:px-6 xl:flex-none xl:px-24">
                 <div className="mx-auto w-full md:max-w-[638px] lg:max-w-[409px]">
                     <div className="w-full mx-auto space-y-8">{children}</div>
                 </div>
             </div>
             <div
-                className="relative hidden rounded-2xl md:px-[52px] lg:w-0 lg:flex-1 md:block bg-[#1e1e1e] lg:overflow-hidden">
-                <div className="flex justify-center h-full items-center">
-                    <MegatraderScreen/>
-                </div>
+                className="relative hidden rounded-2xl md:px-[52px] xl:w-0 lg:flex-1 md:block bg-[#1e1e1e] lg:overflow-hidden pt-16 pb-20">
+                <MegatraderScreen/>
             </div>
         </div>
     );
--- src/components/MegatraderScreen.tsx
+++ src/components/MegatraderScreen.tsx
@@ -3,125 +3,133 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "../app/slick-theme.css";
 
-import React, {useRef} from "react";
+import React, {useEffect, useRef} from "react";
 import Image from "next/image";
+import clsx from "clsx";
+
+const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+};
+
+const sliders = [
+    {
+        imageUrl: '/assets/images/screenshots/ac2.svg',
+        title: 'Power up your trading with full control.',
+        subtitle: 'Log in to manage your accounts, track performance, and unlock the full potential of your MegaTrader journey.'
+    },
+    {
+        imageUrl: '/assets/images/screenshots/affiliates2.svg',
+        title: 'Track, Grow, and Earn with Confidence',
+        subtitle: 'Monitor your referrals, commissions, and performance using powerful tools designed to maximize your affiliate success.'
+    },
+    {
+        imageUrl: '/assets/images/screenshots/payouts2.svg',
+        title: 'Stay Fully in Control of Every Payout',
+        subtitle: 'View your earnings, check payment status, and stay in control of your withdrawals with clear, real-time updates.'
+    },
+]
+
+const SliderItem = ({imageUrl, className, title, subtitle}: {
+    imageUrl: string,
+    className: string,
+    title: string,
+    subtitle: string
+}) => {
+    return <div className={clsx("text-white w-full", className)}>
+        <div className="flex flex-col items-center justify-center w-full h-full">
+            <div className="xl:min-w-3/5">
+                <Image
+                    src={imageUrl}
+                    alt="Account overview"
+                    width={684}
+                    height={659}
+                    style={{width: '100%', height: 'auto'}}
+                    quality={100}
+                />
+            </div>
+            <div className="space-y-2 text-center  mt-[44px]">
+                <h3 className="h-[29px] text-center text-white text-xl font-medium uppercase leading-normal">
+                    {title}
+                </h3>
+                <p className="mx-auto text-stone-400 text-base font-medium leading-normal max-w-[500px] text-center">
+                    {subtitle}
+                </p>
+            </div>
+        </div>
+    </div>
+}
 
 const MegatraderScreen = () => {
     const windowExampleRef = useRef<HTMLInputElement | null>(null);
+    const sliderRef = useRef<Slider | null>(null);
 
-    // useEffect(() => {
-    //     const handlerResize = () => {
-    //         if (!windowExampleRef.current) {
-    //             return;
-    //         }
-    //
-    //         const slickSlider = windowExampleRef.current.querySelector('.slick-slider') as HTMLDivElement;
-    //
-    //         if (!slickSlider) {
-    //             return;
-    //         }
-    //
-    //         const height = slickSlider.offsetHeight;
-    //
-    //         // windowExampleRef.current.style.setProperty('--height-slider', `${height}px`);
-    //     }
-    //
-    //     const observer = new ResizeObserver(handlerResize);
-    //
-    //     if (typeof window !== "undefined") {
-    //         handlerResize();
-    //         observer.observe(document.body);
-    //         window.addEventListener("resize", handlerResize);
-    //     }
-    //
-    //     return () => {
-    //         observer.disconnect();
-    //         window.removeEventListener("resize", handlerResize);
-    //     }
-    // }, []);
+    useEffect(() => {
+        const handlerResize = () => {
+            if (!windowExampleRef.current) {
+                return;
+            }
 
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        autoplay: false,
-        autoplaySpeed: 3000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-    };
+            const slickSlider = windowExampleRef.current.querySelector('.slick-slider') as HTMLDivElement;
 
-    return <div ref={windowExampleRef}
-                className="mx-auto relative xl:w-full">
-        <Slider {...settings}>
-            <div className="text-white h-[var(--height-slider)] flex flex-col justify-center items-center w-full">
-                <div className="flex flex-col items-center justify-center w-full h-full">
-                    <div className="size-9/12 justify-center flex">
-                        <Image
-                            src="/assets/images/screenshots/ac2.svg"
-                            alt="Account overview"
-                            width={684}
-                            height={659}
-                            style={{width: '100%', height: 'auto'}}
-                            quality={100}
-                        />
-                    </div>
-                    <div className="space-y-2 text-center  mt-[44px]">
-                        <h3 className="h-[29px] text-center text-white text-xl font-medium uppercase leading-normal">
-                            Power up your trading with full control.
-                        </h3>
-                        <p className="mx-auto text-stone-400 text-base font-medium leading-normal max-w-[500px] text-center">
-                            Log in to manage your accounts, track performance, and unlock the full potential of your
-                            MegaTrader journey.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div className="text-white h-[var(--height-slider)] flex flex-col justify-center items-center w-full">
-                <div className="flex flex-col items-center justify-center w-full h-full">
-                    <div className="size-9/12 justify-center flex">
-                        <Image
-                            src="/assets/images/screenshots/affiliates2.svg"
-                            alt="Affiliates"
-                            width={684}
-                            height={659}
-                            style={{width: '100%', height: 'auto'}}
-                            quality={100}
-                        />
-                    </div>
-                    <div className="space-y-2 mt-[44px] text-center">
-                        <h3 className="h-[29px] text-center text-white text-xl font-medium uppercase leading-normal">
-                            Track, Grow, and Earn with Confidence
-                        </h3>
-                        <p className="mx-auto text-stone-400 text-base font-medium leading-normal max-w-[500px] text-center">
-                            Monitor your referrals, commissions, and performance using powerful tools designed to
-                            maximize your affiliate success.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div className="text-white h-[var(--height-slider)] flex flex-col justify-center items-center w-full">
-                <div className="flex flex-col items-center justify-center w-full h-full">
-                    <div className="size-9/12 justify-center flex">
-                        <Image
-                            src="/assets/images/screenshots/payouts2.svg"
-                            alt="Affiliates"
-                            width={684}
-                            height={659}
-                            style={{width: '100%', height: 'auto'}}
-                            quality={100}
-                        />
-                    </div>
-                    <div className="space-y-2 mt-[44px] text-center">
-                        <h3 className="h-[29px] text-center text-white text-xl font-medium uppercase leading-normal">
-                            Stay Fully in Control of Every Payout
-                        </h3>
-                        <p className="mx-auto text-stone-400 text-base font-medium leading-normal max-w-[500px] text-center">
-                            View your earnings, check payment status, and stay in control of your withdrawals with
-                            clear, real-time updates.
-                        </p>
-                    </div>
-                </div>
-            </div>
+            if (!slickSlider) {
+                return;
+            }
+
+            const height = slickSlider.offsetHeight;
+
+            console.info('height', height);
+            // windowExampleRef.current.style.setProperty('--height-slider', `${height}px`);
+        }
+
+        const observer = new ResizeObserver(handlerResize);
+
+        if (typeof window !== "undefined") {
+            handlerResize();
+            observer.observe(document.body);
+            window.addEventListener("resize", handlerResize);
+        }
+
+        return () => {
+            observer.disconnect();
+            window.removeEventListener("resize", handlerResize);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (!sliderRef.current) {
+            return;
+        }
+
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                console.info('entry.isIntersecting', entry.isIntersecting);
+            },
+            {threshold: 0.1, root: windowExampleRef.current}
+        );
+
+        return () => {
+            observer.disconnect();
+        }
+    }, [sliderRef.current, windowExampleRef.current]);
+
+
+    return <div ref={windowExampleRef} className="windowExampleRef mx-auto relative xl:w-full">
+        <Slider ref={sliderRef} {...settings}>
+            {sliders.map((sliderItem, index) => (
+                <SliderItem
+                    key={index}
+                    className={`slider-${index + 1}`}
+                    imageUrl={sliderItem.imageUrl}
+                    title={sliderItem.title}
+                    subtitle={sliderItem.subtitle}
+                />
+            ))}
         </Slider>
     </div>
 }
