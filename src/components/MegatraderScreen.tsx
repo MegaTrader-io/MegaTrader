ec5f6ef3dc4b3d8242eb5fad6fddc5f7dff68ef1
Luis Viera <levieraf@gmail.com>
2025-05-01 23:28:07 -0500

    wip

--- src/components/MegatraderScreen.tsx
+++ src/components/MegatraderScreen.tsx
@@ -36,6 +36,7 @@ const MegatraderScreen = () => {
             delay: 3000,
             disableOnInteraction: false
         }}
+                loop={true}
                 pagination={true}
                 modules={[Pagination, Autoplay]}
                 className="h-full pb-[32px]">
