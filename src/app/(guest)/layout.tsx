5b38b1a3b1f953570d6a38472d2e6d3c28d07978
Luis Viera <levieraf@gmail.com>
2025-05-04 21:04:08 -0500

    improve for diferents screens on login

--- src/app/(guest)/layout.tsx
+++ src/app/(guest)/layout.tsx
@@ -3,17 +3,19 @@ import MegatraderScreen from "@/components/MegatraderScreen";
 
 const Layout = ({children}: { children: React.ReactNode }) => {
     return (
-        <div className="xl:h-dvh m-4 xl:flex xl:flex-1">
+        <div className="m-4 xl:max-h-[calc(100dvh-32px)] xl:h-dvh xl:flex xl:flex-row flex-col">
             <div
-                className="flex h-full md:h-auto w-full xl:w-2/5 flex-col justify-center px-4 py-12 sm:px-6 xl:flex-none xl:px-24">
+                className="flex lg:min-h-auto h-full md:h-auto w-full xl:w-2/5 flex-col justify-center px-4 py-12 sm:px-6 xl:flex-none xl:px-24">
                 <div className="mx-auto w-full md:max-w-[638px] lg:max-w-[409px]">
                     <div className="w-full mx-auto space-y-8">{children}</div>
                 </div>
             </div>
             <div
-                className="relative hidden rounded-2xl h-[calc(100dvh-128px-44px-60px)] xl:h-auto md:px-[52px] xl:w-0 lg:flex-1 md:block bg-[#1e1e1e] lg:overflow-hidden">
-                <div className="h-full pt-[60px]">
-                    <MegatraderScreen/>
+                className="relative hidden rounded-2xl pt-[60px] xl:pb-[32px] h-full lg:min-h-auto xl:h-auto md:px-[52px] lg:flex-1 md:block bg-[#1e1e1e] lg:overflow-hidden xl:flex flex-col">
+                <div className="flex items-center min-h-[calc(50dvh-60px-16px-32px)] justify-center flex-grow">
+                    <div className="h-full w-full content-center">
+                        <MegatraderScreen/>
+                    </div>
                 </div>
             </div>
         </div>
@@ -21,3 +23,5 @@ const Layout = ({children}: { children: React.ReactNode }) => {
 };
 
 export default Layout;
+
+
--- src/components/MegatraderScreen.tsx
+++ src/components/MegatraderScreen.tsx
@@ -1,14 +1,12 @@
 'use client'
 import {Swiper, SwiperSlide} from 'swiper/react';
-import {Pagination, Autoplay} from 'swiper/modules';
+import {Pagination} from 'swiper/modules';
 import 'swiper/swiper-bundle.css';
 import 'swiper/css/pagination';
 import '../app/swiper-pagination.css'
 
-import React, {useRef} from "react";
+import React from "react";
 import Image from "next/image";
-import clsx from "clsx";
-
 
 const sliders = [
     {
@@ -29,45 +27,38 @@ const sliders = [
 ]
 
 const MegatraderScreen = () => {
-    const windowExampleRef = useRef<HTMLInputElement | null>(null);
-
-    return <div ref={windowExampleRef} className="windowExampleRef h-full">
-        <Swiper autoplay={{
-            delay: 3000,
-            disableOnInteraction: false
-        }}
-                loop={true}
-                pagination={true}
-                modules={[Pagination, Autoplay]}
-                className="h-full pb-[32px]">
-            {sliders.map(({imageUrl, title, subtitle}, index) => (
-                <SwiperSlide key={index} className={'h-full'}>
-                    <div className={clsx("text-white w-full h-full")}>
-                        <div
-                            className="flex-col w-full h-full xl:items-center 2xl:items-start xl:grid xl:grid-rows-[auto_128px]">
-                            <Image
-                                src={imageUrl}
-                                className="size-3/4 md:max-h-[640px] xl:max-h-[calc(100dvh-60px-85px-32px-128px)]"
-                                alt="Account overview"
-                                width={684}
-                                height={659}
-                                style={{width: '100%', height: 'auto'}}
-                                quality={100}
-                            />
-                            <div className="space-y-2 text-center relative md:top-[44px] xl:-top-[44px]">
-                                <h3 className="h-[29px] text-center text-white text-xl font-medium uppercase leading-normal">
-                                    {title}
-                                </h3>
-                                <p className="mx-auto text-stone-400 text-base font-medium leading-normal max-w-[500px] text-center">
-                                    {subtitle}
-                                </p>
-                            </div>
-                        </div>
+    return <Swiper autoplay={{
+        delay: 3000,
+        disableOnInteraction: false
+    }}
+                   loop={true}
+                   pagination={true}
+                   modules={[Pagination]}>
+        {sliders.map(({imageUrl, title, subtitle}, index) => (
+            <SwiperSlide key={index}>
+                <div
+                    className="min-h-[773px] flex-col w-full h-full xl:items-center 2xl:items-start xl:grid xl:grid-rows-[auto_128px]">
+                    <Image
+                        src={imageUrl}
+                        className="size-3/4 md:max-h-[640px] xl:max-h-[calc(100dvh-60px-85px-32px-128px-32px)]"
+                        alt="Account overview"
+                        width={684}
+                        height={659}
+                        style={{objectFit: 'fill', width: '100%', height: '100%'}}
+                        quality={100}
+                    />
+                    <div className="space-y-2 text-center relative md:top-[44px]">
+                        <h3 className="h-[29px] text-center text-white text-xl font-medium uppercase leading-normal">
+                            {title}
+                        </h3>
+                        <p className="mx-auto text-stone-400 text-base font-medium leading-normal max-w-[500px] text-center">
+                            {subtitle}
+                        </p>
                     </div>
-                </SwiperSlide>
-            ))}
-        </Swiper>
-    </div>
+                </div>
+            </SwiperSlide>
+        ))}
+    </Swiper>
 }
 
 export default MegatraderScreen;
