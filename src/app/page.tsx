a7603a4fa9ef88616fec341caee2a8e05ad90122
Luis Viera <levieraf@gmail.com>
2025-05-06 14:14:12 -0500

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,7 +69,7 @@ const Home = () => {
     }
 
     return <HomeLayout>
-        <div className="mx-auto sm:w-[406px] space-y-8 h-[calc(100dvh-64px)] content-center sm:h-full sm:content-normal">
+        <div className="mx-auto sm:w-[406px] xl:w-[406px] space-y-8 h-[calc(100dvh-64px)] content-center sm:h-full sm:content-normal">
             {showAlert && (
                 <div className="w-full">
                     <Alert type={showAlert.type}
--- src/components/HomeLayout.tsx
+++ src/components/HomeLayout.tsx
@@ -41,7 +41,7 @@ const Layout = ({children}: { children: React.ReactNode }) => {
         <div className="m-4 xl:max-h-[calc(100dvh-32px)] xl:h-dvh xl:flex xl:flex-row flex-col">
             <div
                 ref={divRef}
-                className="mg-divRef flex h-full w-full flex-col justify-center sm:py-12 sm:px-6 md:h-auto lg:min-h-auto xl:w-2/5 xl:flex-none xl:px-24">
+                className="mg-divRef flex h-full w-full flex-col justify-center sm:py-12 sm:pr-6 md:h-auto lg:min-h-auto xl:w-2/5 xl:flex-none xl:pr-4 2xl:px-24">
                 <div className="mx-auto w-full">
                     {children}
                 </div>
