43332e2d9aa2376b16c770715d6c314177195527
Luis Viera <levieraf@gmail.com>
2025-05-22 17:42:36 -0500

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,60 +5,120 @@ import { roboto } from "@/app/fonts/roboto";
 import { LoadingProvider } from "@/context/LoadingContext";
 import LoadingOverlay from "@/components/LoadingOverlay";
 import NavigationLoader from "@/components/NavigationLoader";
-import { LoadingBetweenPagesProvider } from "@/context/LoadingBetweenPagesContext";
+import {
+    LoadingBetweenPagesProvider,
+} from "@/context/LoadingBetweenPagesContext";
 import LoadingBetweenPagesOverlay from "@/components/LoadingBetweenPagesOverlay";
 
 export const metadata: Metadata = {
     title: "MegaTrader | Master the Path to Becoming a Funded Futures Trader.",
     description: "MegaTrader | Master the Path to Becoming a Funded Futures Trader.",
     icons: {
-        icon: "/favicon.svg"
-    }
+        icon: "/favicon.svg",
+    },
 };
 
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 const isProduction = process.env.NEXT_PUBLIC_ENVIRONMENT === "production";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+                                       children,
+                                   }: {
+    children: React.ReactNode;
+}) {
+    if (isProduction && !GTM_ID) {
+        console.warn(
+            "[GTM] NEXT_PUBLIC_GTM_ID no está definido. Revisa tu .env.local"
+        );
+    }
+
     return (
         <html lang="en" className={`${roboto.variable} dark`}>
         <head>
-            {isProduction && (
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement('script'),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K7TW2RR7');`
-                    }}
+            {/* Otros meta tags generados por Next.js */}
+            {isProduction && GTM_ID && (
+                // Carga asíncrona del script externo
+                <Script
+                    src={`https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`}
+                    strategy="afterInteractive"
                 />
             )}
+
+            {isProduction && GTM_ID && (
+                // Inyecta el snippet inline que inicializa dataLayer
+                <Script id="gtm-init" strategy="afterInteractive">
+                    {`
+              window.dataLayer = window.dataLayer || [];
+              window.dataLayer.push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+              });
+            `}
+                </Script>
+            )}
         </head>
+
         <body className="font-roboto antialiased">
-        {isProduction && (
+        {isProduction && GTM_ID && (
             <noscript>
                 <iframe
-                    src="https://www.googletagmanager.com/ns.html?id=GTM-K7TW2RR7"
+                    src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
                     height="0"
                     width="0"
                     style={{ display: "none", visibility: "hidden" }}
                 />
             </noscript>
         )}
+
         <LoadingBetweenPagesProvider>
             <NavigationLoader />
             <LoadingBetweenPagesOverlay />
+
             <LoadingProvider>
                 <LoadingOverlay />
                 {children}
+
+                {/* Script de Intercom, también con <Script> */}
                 {isProduction && (
-                    <Script id="livechat-script" strategy="lazyOnload">
+                    <Script id="intercom" strategy="lazyOnload">
                         {`
-(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/izt54gd4';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-window.intercomSettings = {
-api_base: "https://api-iam.intercom.io",
-app_id: "izt54gd4",
-user_id: "123",
-name: "foo bar",
-email: "test@megatrader.io",
-created_at: new Date().getTime(),
-};
+                  (function(){
+                    var w = window;
+                    var ic = w.Intercom;
+                    if (typeof ic === "function") {
+                      ic("reattach_activator");
+                      ic("update", w.intercomSettings);
+                    } else {
+                      var d = document;
+                      var i = function(){ i.c(arguments); };
+                      i.q = [];
+                      i.c = function(args){ i.q.push(args); };
+                      w.Intercom = i;
+                      var l = function(){
+                        var s = d.createElement("script");
+                        s.type = "text/javascript";
+                        s.async = true;
+                        s.src = "https://widget.intercom.io/widget/izt54gd4";
+                        var x = d.getElementsByTagName("script")[0];
+                        x.parentNode.insertBefore(s, x);
+                      };
+                      if (document.readyState === "complete") {
+                        l();
+                      } else if (w.attachEvent) {
+                        w.attachEvent("onload", l);
+                      } else {
+                        w.addEventListener("load", l, false);
+                      }
+                    }
+                  })();
+                  window.intercomSettings = {
+                    api_base: "https://api-iam.intercom.io",
+                    app_id: "izt54gd4",
+                    user_id: "123",
+                    name: "foo bar",
+                    email: "test@megatrader.io",
+                    created_at: new Date().getTime()
+                  };
                 `}
                     </Script>
                 )}
