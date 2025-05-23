34f9daeaf9f4db7f51bdb7251e8ee0ae2e9f5b6a
Luis Viera <levieraf@gmail.com>
2025-05-23 16:56:12 -0500

    wip

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import {NextConfig} from 'next';
 
-
 const isProduction = process.env.NEXT_PUBLIC_ENVIRONMENT === "production";
-
 
 const nextConfig: NextConfig = {
     images: {
@@ -23,8 +21,9 @@ const nextConfig: NextConfig = {
                         {
                             key: 'Content-Security-Policy',
                             value: `
-                                script-src 'self' 'unsafe-inline' https://cdn.livechatinc.com https://api.livechatinc.com https://widget.intercom.io https://js.intercomcdn.com;
+                                script-src 'self' 'unsafe-inline' https://cdn.livechatinc.com https://api.livechatinc.com https://widget.intercom.io https://js.intercomcdn.com https://www.googletagmanager.com;
                                 object-src 'none';
+                                frame-src 'self' https://www.googletagmanager.com;
                                 frame-ancestors 'self';
                                 connect-src 'self' https://api.livechatinc.com https://api-iam.intercom.io https://js.intercomcdn.com wss://nexus-websocket-a.intercom.io;
                             `.replace(/\s{2,}/g, ' ').trim(),
@@ -51,4 +50,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
