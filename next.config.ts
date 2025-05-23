60c09418f7af2bd1090f84d4cf11db376a576ce0
Luis Viera <levieraf@gmail.com>
2025-05-23 17:25:38 -0500

    wip

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
-import {NextConfig} from 'next';
+import { NextConfig } from 'next';
 
-const isProduction = process.env.NEXT_PUBLIC_ENVIRONMENT === "production";
+const isProduction = process.env.NEXT_PUBLIC_ENVIRONMENT === 'production';
 
 const nextConfig: NextConfig = {
     images: {
@@ -21,12 +21,12 @@ const nextConfig: NextConfig = {
                         {
                             key: 'Content-Security-Policy',
                             value: `
-                                script-src 'self' 'unsafe-inline' https://cdn.livechatinc.com https://api.livechatinc.com https://widget.intercom.io https://js.intercomcdn.com https://www.googletagmanager.com;
-                                object-src 'none';
-                                frame-src 'self' https://www.googletagmanager.com;
-                                frame-ancestors 'self';
-                                connect-src 'self' https://api.livechatinc.com https://api-iam.intercom.io https://js.intercomcdn.com wss://nexus-websocket-a.intercom.io;
-                            `.replace(/\s{2,}/g, ' ').trim(),
+                script-src 'self' 'unsafe-inline' https://cdn.livechatinc.com https://api.livechatinc.com https://widget.intercom.io https://js.intercomcdn.com https://www.googletagmanager.com https://connect.facebook.net https://googleads.g.doubleclick.net;
+                object-src 'none';
+                frame-src 'self' https://www.googletagmanager.com https://td.doubleclick.net;
+                frame-ancestors 'self';
+                connect-src 'self' https://api.livechatinc.com https://api-iam.intercom.io https://js.intercomcdn.com wss://nexus-websocket-a.intercom.io https://www.google.com;
+              `.replace(/\s{2,}/g, ' ').trim(),
                         },
                         {
                             key: 'Cache-Control',
@@ -44,7 +44,6 @@ const nextConfig: NextConfig = {
                 },
             ];
         }
-
         return [];
     },
 };
