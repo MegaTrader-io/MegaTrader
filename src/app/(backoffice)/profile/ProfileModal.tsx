fd71ef752be4b7c4df48cddfe91eaa9c6f32b254
Luis Viera <levieraf@gmail.com>
2025-06-06 12:49:28 -0500

    wip

--- src/app/(backoffice)/profile/ProfileModal.tsx
+++ src/app/(backoffice)/profile/ProfileModal.tsx
@@ -17,8 +17,8 @@ function ProfileModal() {
 
     return (
         <Dialog
-            className="w-[calc(100vw-32px)] sm:max-w-[1024px] px-4"
-            childrenClassName="px-0"
+            className="w-[calc(100vw-32px)] sm:max-w-[1024px] px-4 py-8"
+            childrenClassName="px-0 pb-0"
             showModal={showModal}
             onClose={handleCloseDialog}
             title={'MY PROFILE'}>
