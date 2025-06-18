c8bb50013a6676e07ea83d5483c5c6340bd65882
Luis Viera <levieraf@gmail.com>
2025-06-17 19:22:06 -0500

    wip

--- src/components/CustomBarChar.tsx
+++ src/components/CustomBarChar.tsx
@@ -45,7 +45,7 @@ const CustomBarChar: React.FC<ChartBarProps> = ({internalId, xAxis, series, yAxi
         const marginLeft = 40;
         panelBars.style.marginLeft = `${marginLeft}px`;
         panelBars.style.width = `${panelBars.style.width || 0 - marginLeft}px`;
-        panelLines.style.width = `${panelChartWrapper.scrollWidth + maxWidthLabel - 17 - 8}px`;
+        panelLines.style.width = `${panelChartWrapper.scrollWidth + maxWidthLabel - 16 - 8}px`;
     };
 
     useEffect(() => {
@@ -60,7 +60,7 @@ const CustomBarChar: React.FC<ChartBarProps> = ({internalId, xAxis, series, yAxi
     }, [internalId]);
 
     if (!series?.length || !xAxis?.length) {
-        return <NoData />
+        return <NoData/>
     }
 
     function niceNumber(x: number, round: boolean): number {
