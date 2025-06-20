45a1737f43d8e6793292361438a8ca418e57ae69
Luis Viera <levieraf@gmail.com>
2025-06-19 22:06:06 -0500

    wip

--- src/app/api/user/me/route.ts
+++ src/app/api/user/me/route.ts
@@ -11,7 +11,7 @@ export async function GET() {
     const userId = 123;
     const email = 'test@megatrader.io';
     const name = 'foo bar';
-    const phone = '+50760036763'//'+13054310620';
+    const phone = '+13054310620';
     const createdAt = dayjs().utc().unix()
 
     const user = {
