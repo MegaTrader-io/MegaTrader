9075f07a2bbfd70003037ce694f2634c029fe4c8
Luis Viera <levieraf@gmail.com>
2025-06-28 10:50:27 -0500

    wip

--- src/components/landing-page/EasyWayToBecomeAFundedTrader.tsx
+++ src/components/landing-page/EasyWayToBecomeAFundedTrader.tsx
@@ -16,51 +16,132 @@ const EasyWayToBecomeAFundedTrader: NextPage = () => (
             earning with confidence and clarity.
         </div>
 
-        <div className="grid grid-cols-2 gap-16">
-            <div className="flex justify-center">
-                <PlanVerticalList/>
-            </div>
-            <div className="flex items-center">
-                <div className="w-full max-w-[321px] lg:max-w-[585px]">
-                    <div
-                        className="justify-start text-white text-[32px] font-medium uppercase leading-10">
-                        01
-                    </div>
-                    <div
-                        className="justify-start text-[#ffb34a] text-xl font-medium leading-loose">Select
-                        Your Plan & Account Size
-                    </div>
-                    <div
-                        className="justify-start text-stone-400 text-base font-medium leading-normal">Choose
-                        the right plan that fits your trading style and goals. Pick your preferred account size and take
-                        on
-                        the challenge to become a funded trader.
+        <div>
+            <div className="grid grid-cols-2 gap-[78px]">
+                <div className="flex justify-center">
+                    <PlanVerticalList/>
+                </div>
+                <div className="flex items-center">
+                    <div className="w-full max-w-[321px] lg:max-w-[585px]">
+                        <div
+                            className="justify-start text-white text-[32px] font-medium uppercase leading-10">
+                            01
+                        </div>
+                        <div
+                            className="justify-start text-[#ffb34a] text-xl font-medium leading-loose">Select
+                            Your Plan & Account Size
+                        </div>
+                        <div
+                            className="justify-start text-stone-400 text-base font-medium leading-normal">Choose
+                            the right plan that fits your trading style and goals. Pick your preferred account size and
+                            take
+                            on
+                            the challenge to become a funded trader.
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="flex items-center">
-                <div className="max-w-[585px] text-right">
-                    <div
-                        className="justify-start text-white text-[32px] font-medium uppercase leading-10">
-                        02
+            <div className="grid grid-cols-2 gap-[78px]">
+                <div className="flex items-center">
+                    <div className="max-w-[585px] text-right">
+                        <div
+                            className="justify-start text-white text-[32px] font-medium uppercase leading-10">
+                            02
+                        </div>
+                        <div
+                            className="justify-start text-[#ffb34a] text-xl font-medium leading-loose">Select
+                            Complete the Evaluation Challenge
+                        </div>
+                        <div
+                            className="justify-start text-stone-400 text-base font-medium leading-normal">
+                            Trade responsibly and meet the required profit targets while following risk management
+                            rules.
+                            Once you succeed, you can move on to the next step.
+                        </div>
                     </div>
-                    <div
-                        className="justify-start text-[#ffb34a] text-xl font-medium leading-loose">Select
-                        Complete the Evaluation Challenge
-                    </div>
-                    <div
-                        className="justify-start text-stone-400 text-base font-medium leading-normal">
-                        Trade responsibly and meet the required profit targets while following risk management rules.
-                        Once you succeed, you can move on to the next step.
+                </div>
+                <div className="flex justify-center">
+                    <Card className="w-full max-w-[585px] pb-0 flex justify-center border-b-0">
+                        <Image src={`/assets/images/Window-congrats.svg`} alt="veriff" width={440} height={543}/>
+                    </Card>
+                </div>
+            </div>
+            <div className="grid grid-cols-2 gap-[78px]">
+                <div className="flex justify-center">
+                    <Card className="w-full max-w-[585px] pb-0 flex justify-center border-b-0 relative">
+                        <Image src={`/assets/images/Window-request-payouts.svg`} alt="veriff" width={360} height={524}/>
+                        <Image src={`/assets/images/icons.png`} alt="veriff" className="absolute right-4 bottom-4" width={160} height={48}/>
+                    </Card>
+                </div>
+                <div className="flex items-center">
+                    <div className="max-w-[585px] text-left">
+                        <div
+                            className="justify-start text-white text-[32px] font-medium uppercase leading-10">
+                            03
+                        </div>
+                        <div
+                            className="justify-start text-[#ffb34a] text-xl font-medium leading-loose">
+                            Request Your First Payout
+                        </div>
+                        <div
+                            className="justify-start text-stone-400 text-base font-medium leading-normal">
+                            Once you’ve passed the challenge and made profits in your funded account, submit a payout
+                            request. This step initiates the verification process.
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="flex justify-center">
-                <Card className="w-full max-w-[585px] pb-0 flex justify-center border-b-0">
-                    <Image src={`/assets/images/Window-congrats.svg`} alt="veriff" width={440} height={543}/>
-                </Card>
+            <div className="grid grid-cols-2 gap-[78px]">
+                <div className="flex items-center">
+                    <div className="max-w-[585px] text-right">
+                        <div
+                            className="justify-start text-white text-[32px] font-medium uppercase leading-10">
+                            04
+                        </div>
+                        <div
+                            className="justify-start text-[#ffb34a] text-xl font-medium leading-loose">
+                            Get Verified
+                        </div>
+                        <div
+                            className="justify-start text-stone-400 text-base font-medium leading-normal">
+                            Before receiving your payout, you’ll go through a quick verification process to confirm your
+                            identity and compliance with the platform’s rules
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-center">
+                    <Card className="w-full max-w-[585px] pb-0 flex justify-center border-b-0">
+                        <Image src={`/assets/images/Window-selfie-veriff.svg`} alt="veriff" width={440} height={543}/>
+                    </Card>
+                </div>
+            </div>
+            <div className="grid grid-cols-2 gap-[78px]">
+                <div className="flex justify-center">
+                    <div className="w-full max-w-[585px] pb-0 flex justify-center">
+                        <Image src={`/assets/images/card-get-paid-keep-trading.svg`} alt="veriff" width={585}
+                               height={457}/>
+                    </div>
+                </div>
+                <div className="flex items-center">
+                    <div className="max-w-[585px] text-left">
+                        <div
+                            className="justify-start text-white text-[32px] font-medium uppercase leading-10">
+                            05
+                        </div>
+                        <div
+                            className="justify-start text-[#ffb34a] text-xl font-medium leading-loose">
+                            Get Paid & Keep Trading
+                        </div>
+                        <div
+                            className="justify-start text-stone-400 text-base font-medium leading-normal">
+                            Once verified, your payout is processed, and you can continue trading. Earn up to a 90%
+                            profit split while scaling your funded account!
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+
     </section>
 );
 
