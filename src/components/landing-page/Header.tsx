6b37d152438c92c352c0f7e4f6114164f641f392
Luis Viera <levieraf@gmail.com>
2025-06-30 12:02:49 -0500

    wip

--- src/components/landing-page/Header.tsx
+++ src/components/landing-page/Header.tsx
@@ -19,6 +19,7 @@ export default function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const currentPath = usePathname()
 
+    console.info(hasScrolled);
     useEffect(() => {
         const handleScroll = () => {
             if (window.scrollY > 10) {
