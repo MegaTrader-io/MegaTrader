5b3070e8202350856167b92b1b700ad93706cdab
Luis Viera <levieraf@gmail.com>
2025-07-02 15:08:51 -0500

    wip

--- src/app/landing-page/page.tsx
+++ src/app/landing-page/page.tsx
@@ -19,8 +19,23 @@ import DiscoverThePlatformsPoweringYourTrades from "@/components/landing-page/Di
 import OurWithDrawalMethods from "@/components/landing-page/OurWithDrawalMethods";
 import TrustedByLeadres from "@/components/landing-page/TrustedByLeadres";
 import GetTheAnswersYouNeed from "@/components/landing-page/GetTheAnswersYouNeed";
+import {Button} from "@/components/Button";
 
 const Page = () => {
+
+    function scrollToTop() {
+        try {
+            if (typeof window !== 'undefined') {
+                window.scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
+                });
+            }
+        } catch (error) {
+            console.error('unable to scroll to the top:', error);
+        }
+    }
+
     return <>
         <header id="home" className="px-4 w-full z-50">
             <Header/>
@@ -56,7 +71,6 @@ const Page = () => {
                         className="w-[1280px] h-auto hidden lg:block"
                     />
                 </div>
-
                 <Suspense fallback={'loading...'}>
                     <MarketOverviewSection/>
                 </Suspense>
@@ -72,7 +86,25 @@ const Page = () => {
             <OurWithDrawalMethods/>
             <TrustedByLeadres/>
             <GetTheAnswersYouNeed/>
-            <div className="pb-12"></div>
+            <div className="flex px-4 justify-end !my-8">
+                <Button onClick={scrollToTop} styleType={'filled'} variant={'light'} className="!pl-3 !pr-4">
+                    <div className="flex gap-2">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <mask id="mask0_11468_2063" style={{maskType: 'alpha'}} maskUnits="userSpaceOnUse" x="0"
+                                  y="0"
+                                  width="24" height="24">
+                                <rect width="24" height="24" fill="#D9D9D9"/>
+                            </mask>
+                            <g mask="url(#mask0_11468_2063)">
+                                <path d="M11 18V8.8L7.4 12.4L6 11L12 5L18 11L16.6 12.4L13 8.8V18H11Z" fill="black"/>
+                            </g>
+                        </svg>
+                        <div>
+                            BACK TO TOP
+                        </div>
+                    </div>
+                </Button>
+            </div>
         </main>
         <Footer/>
     </>
