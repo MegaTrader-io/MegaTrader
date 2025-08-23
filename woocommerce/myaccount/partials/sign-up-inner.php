949c88ab5c50f2395c12102446eb2ecf3d5f4dd9
Luis Viera <levieraf@gmail.com>
2025-08-23 14:32:57 -0500

    fix

--- woocommerce/myaccount/partials/sign-up-inner.php
+++ woocommerce/myaccount/partials/sign-up-inner.php
@@ -39,6 +39,7 @@ foreach ($errors as $error) {
 }
 
 $default_country = 'US';
+
 $valid_states = WC()->countries->get_states($default_country);
 ?>
 
@@ -273,11 +274,14 @@ $valid_states = WC()->countries->get_states($default_country);
         <?php endif; ?>
     </div>
     <?php do_action('woocommerce_register_form'); ?>
-
-    <div class="google-signin-btn">
-        <div class="googlesitekit-sign-in-with-google__frontend-output-button">
-            <!-- Here's where googlesitekit injects btn iframe --></div>
-    </div>
+    <p class="">
+        <?php wp_nonce_field('woocommerce-register', 'woocommerce-register-nonce'); ?>
+        <button type="submit"
+                class="btn w-100 mega-btn-md mega-btn-primary-md w-100 <?php echo esc_attr(wc_wp_theme_get_element_class_name('button') ? ' ' . wc_wp_theme_get_element_class_name('button') : ''); ?>"
+                name="register"
+                value="<?php esc_attr_e('Register', 'woocommerce'); ?>"><?php esc_html_e('REGISTER', 'woocommerce'); ?>
+        </button>
+    </p>
 
     <?php do_action('woocommerce_login_form_end'); ?>
 </form>
