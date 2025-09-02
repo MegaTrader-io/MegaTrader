2af2d6ae6083d316c702839b168edb8452f9cc35
Luis Viera <levieraf@gmail.com>
2025-09-02 14:07:44 -0500

    adding default header height

--- header-landing-page.php
+++ header-landing-page.php
@@ -1,11 +1,18 @@
-<!DOCTYPE html>
+<?php
+ob_start();
+do_action('mega_sticky_promo_render_banner');
+$sticky_promo_html = ob_get_clean();
+$header_height = empty($sticky_promo_html) ? 100 + 24 : 100 + 24 + 54;
+?>
+
+    <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php wp_head(); ?>
-</head>
-<body <?php body_class('font-roboto antialiased'); ?>>
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php wp_head(); ?>
+    </head>
+<body <?php body_class('font-roboto antialiased'); ?> style="--header-height: <?= $header_height ?>px;">
 <div class="loading-overlay tw-hidden tw-fixed tw-inset-0 tw-bg-[#131210]/90 tw-justify-center tw-items-center tw-z-[9999999]">
     <div class="loader"></div>
 </div>
