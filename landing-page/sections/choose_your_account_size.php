ca5a39a5da76a9748ca2f520f73a74c0654c8c6f
Luis Viera <levieraf@gmail.com>
2025-09-08 12:31:11 -0500

    wip

--- landing-page/sections/choose_your_account_size.php
+++ landing-page/sections/choose_your_account_size.php
@@ -242,10 +242,9 @@ function get_best_selling_product(array $order_statuses = ['wc-completed'], int 
                 <div data-price="<?= $size ?>"
                      class="price-information tw-px-4 tw-flex tw-border-r-2 group-[.last-element]:tw-border-r-0 tw-border-stone-800 <?= $has_coupon_global && !$has_coupon ? 'tw-min-h-[140px] tw-items-center' : '' ?>">
                     <div class="tw-space-y-2 tw-my-3 tw-w-full">
-                        <div style="display: <?= $has_coupon ? 'flex' : 'none' ?>"
+                        <div style="display: <?= $has_coupon ? 'block' : 'none' ?>"
                              data-price="<?= $size ?>"
                              class="badge-coupon tw-w-full tw-flex tw-items-center tw-justify-between tw-gap-2">
-<!--                            tw-px-2 tw-w-full tw-py-1 tw-rounded-lg tw-outline tw-outline-1 tw-outline-offset-[-1px] tw-outline-teal-500 tw-inline-flex tw-justify-start tw-items-center tw-gap-2----->
                             <div class="mt-badge mt-badge-secondary">
                                 <div class="tw-w-full tw-text-base tw-font-medium tw-leading-normal">
                                     Save <span
