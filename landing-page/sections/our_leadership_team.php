2831cf4672c64cb8b245bf7ea3a9dcd5005abf58
Luis Viera <levieraf@gmail.com>
2025-09-12 13:14:19 -0500

    adding new section

--- landing-page/sections/our_leadership_team.php
+++ landing-page/sections/our_leadership_team.php
@@ -10,7 +10,7 @@ $people = [
 
 ?>
 
-<section id="our_team" class="tw-space-y-4 tw-px-8">
+<section id="our_team" class="tw-space-y-4 tw-px-8 tw-pb-12">
     <div class="tw-self-stretch tw-text-center tw-text-white tw-text-[40px] tw-font-light tw-uppercase tw-leading-[48px]">
         Our Leadership & Team
     </div>
@@ -22,21 +22,20 @@ $people = [
             deliver a platform built on fairness, transparency, and trader success worldwide.
         </div>
 
-        <div class="tw-flex tw-justify-center tw-gap-4">
+        <div class="tw-grid tw-grid-cols-1 tw-mx-auto sm:tw-grid-cols-2 md:tw-grid-cols-3 xl:tw-flex xl:tw-justify-center tw-gap-4">
             <?php foreach ($people as $index => $person) : ?>
                 <?php
                 $url = get_template_directory_uri() . "/assets/img/landing-page/team/{$person['image']}";
                 ?>
-                <div class="tw-flex <?= $index % 2 == 0 ? 'tw-flex-col' : ' tw-flex-col-reverse' ?>">
+                <div class="tw-mx-auto md:mx-0 md:tw-flex <?= $index % 2 == 0 ? 'tw-flex-col' : 'tw-flex-col-reverse' ?>">
                     <img src="<?= $url ?>" alt="<?= $person['name'] ?>" class="tw-w-[236px] tw-h-[360px]">
                     <div class="tw-p-2">
                         <div class="tw-justify-start tw-text-teal-400 tw-text-base tw-font-medium tw-leading-normal"><?= $person['name'] ?></div>
                         <div class="tw-justify-start tw-text-stone-400 tw-text-base tw-font-medium tw-leading-normal">
-                            Founder and CEO
+                            <?= $person['role'] ?>
                         </div>
                     </div>
                 </div>
-
             <?php endforeach; ?>
         </div>
     </div>
