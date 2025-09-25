e26d1fcd12f1408e3f9f8d364d3377cd4f2b96b1
Luis Viera <levieraf@gmail.com>
2025-09-25 16:54:15 -0500

    wip

--- template-parts/landing-page/sections/price_table.php
+++ template-parts/landing-page/sections/price_table.php
@@ -183,7 +183,7 @@ $tabs = array_map(function ($item) {
                             <div class="mt-pricing-card__header-text mt-pricing-card--discount_total">Save <span><?= $has_coupon ? mt_price_plain($coupon['discount_total']) : 0 ?></span> with code</div>
                             <div class="mt-pricing-card__code-container">
                                 <span class="mt-pricing-card__code text-uppercase"><?= $has_coupon ? $coupon['coupon'] : 0 ?></span>
-                                <button type="button" class="mt-pricing-card__copy-btn btn-copy" aria-label="Copy Code" data-copy-text="<?= $has_coupon ? $coupon['coupon'] : 0 ?>">
+                                <button type="button" class="mt-pricing-card__copy-btn btn-copy tw-relative" aria-label="Copy Code" data-copy-text="<?= $has_coupon ? $coupon['coupon'] : 0 ?>">
                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <mask id="mask0_15572_33010" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
                                             <rect width="24" height="24" fill="#D9D9D9"/>
@@ -192,6 +192,7 @@ $tabs = array_map(function ($item) {
                                             <path d="M9 18C8.45 18 7.97917 17.8042 7.5875 17.4125C7.19583 17.0208 7 16.55 7 16V4C7 3.45 7.19583 2.97917 7.5875 2.5875C7.97917 2.19583 8.45 2 9 2H18C18.55 2 19.0208 2.19583 19.4125 2.5875C19.8042 2.97917 20 3.45 20 4V16C20 16.55 19.8042 17.0208 19.4125 17.4125C19.0208 17.8042 18.55 18 18 18H9ZM5 22C4.45 22 3.97917 21.8042 3.5875 21.4125C3.19583 21.0208 3 20.55 3 20V6H5V20H16V22H5Z" fill="black"/>
                                         </g>
                                     </svg>
+                                    <span class="tw-absolute -tw-top-[28px] -tw-translate-x-[45%] tw-text-xs !tw-text-white tw-hidden mgt-copy-tooltip">copied</span>
                                 </button>
                             </div>
                         </div>
