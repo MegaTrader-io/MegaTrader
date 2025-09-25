70defa6695372d62138cbce0cd6db5b73e584825
Luis Viera <levieraf@gmail.com>
2025-09-25 18:35:27 -0500

    abstrayendo logica de guillermo para reutilizar url hacia la pantalla correcta cuando el user esta logeado

--- template-parts/my-profile.php
+++ template-parts/my-profile.php
@@ -65,7 +65,7 @@ $logout_url = wp_logout_url();
 
 <div class="mt-my-profile d-flex gap-3 align-items-center justify-content-start">
     <?php if ($go_to_dashboard) : ?>
-        <a href="<?= home_url('/my-account/') ?>" class="avatar-area align-items-center d-flex flex-fill gap-3">
+        <a href="<?= profile_url() ?>" class="avatar-area align-items-center d-flex flex-fill gap-3">
             <?php get_template_part('template-parts/avatar', null, [
                     'avatar_url' => $avatar_url,
                     'avatar_size' => $avatar_size,
