ccebc7693e962cb0c360d8fa8af47625b24cb6ee
Deploy Megatrader <deploy@megatrader.io>
2025-10-03 22:49:19 +0000

    saving changes

--- woocommerce/myaccount/my-account.php
+++ woocommerce/myaccount/my-account.php
@@ -14,7 +14,16 @@ defined('ABSPATH') || exit;
         <div class="mt-page__sidebar">
             <?php render_sidebar(); ?>
         </div>
+
         <div class="mt-page__main">
+            <div class="mt-page__main-header">
+                <div class="mt-page__title text-white text-size-20 fw-medium text-uppercase">
+                    <?php echo Label::META_ACCOUNT_OVERVIEW['page_title_overview']; ?>
+                </div>
+                <span class="mt-page__subtitle text-a8a29e text-14px-line-20px fw-medium">
+                    <?php echo Label::META_ACCOUNT_OVERVIEW['page_subtitle_overview']; ?>
+                </span>
+            </div>
             <div class="mb-3">
                 <?php account_navigation_render(); ?>
             </div>
