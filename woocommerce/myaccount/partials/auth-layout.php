a73a6977f34e741a65ac5a39b548cbb11f4a29b6
Luis Viera <levieraf@gmail.com>
2025-10-16 19:55:45 -0500

    adding slider with images

--- woocommerce/myaccount/partials/auth-layout.php
+++ woocommerce/myaccount/partials/auth-layout.php
@@ -20,9 +20,9 @@ if (!isset($classes) || !is_string($classes)) $classes = '';
                     <div class="swiper-wrapper">
                         <div class="swiper-slide">
                             <div class="slider-content">
-                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/auth/sd1.png"
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/auth/sd11.png"
                                      class="slider-content__image-web"
-                                     style="object-fit: fill"
+                                     style="object-fit: fill;min-height: 685px"
                                      alt="Logo MegaTraderX"/>
 
                                 <div class="slider-content__info">
@@ -38,36 +38,18 @@ if (!isset($classes) || !is_string($classes)) $classes = '';
                         </div>
                         <div class="swiper-slide">
                             <div class="slider-content">
-                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/auth/sd2.png"
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/auth/sd12.png"
                                      class="slider-content__image-web"
-                                     style="object-fit: fill"
+                                     style="object-fit: fill;min-height: 685px"
                                      alt="Logo MegaTraderX"/>
 
                                 <div class="slider-content__info">
                                     <h3 class="slider-content__title mb-0">
-                                        Track, Grow, and Earn with Confidence
+                                        Execute every trade with precision and speed
                                     </h3>
                                     <p class="slider-content__subtitle ">
-                                        Monitor your referrals, commissions, and performance using powerful
-                                        tools designed to maximize your affiliate success.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="slider-content">
-                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/auth/sd3.png"
-                                     class="slider-content__image-web"
-                                     style="object-fit: fill"
-                                     alt="Logo MegaTraderX"/>
-
-                                <div class="slider-content__info">
-                                    <h3 class="slider-content__title mb-0">
-                                        Stay Fully in Control of Every Payout
-                                    </h3>
-                                    <p class="slider-content__subtitle">
-                                        View your earnings, check payment status, and stay in control of your
-                                        withdrawals with clear, real-time updates.
+                                        Access advanced order tools, real-time data, and a seamless interface built for
+                                        serious futures traders.
                                     </p>
                                 </div>
                             </div>
