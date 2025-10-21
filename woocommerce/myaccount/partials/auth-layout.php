75dbb20a06620c4d13230343a5972e2341a64b89
Luis Viera <levieraf@gmail.com>
2025-10-21 17:05:16 -0500

    fix wip slider

--- woocommerce/myaccount/partials/auth-layout.php
+++ woocommerce/myaccount/partials/auth-layout.php
@@ -22,9 +22,8 @@ if (!isset($classes) || !is_string($classes)) $classes = '';
                             <div class="slider-content">
                                 <div class="slider-content__image-wrapper slider-content__image-wrapper--slider-1">
                                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/auth/s1.png"
-                                         style="object-fit: fill;max-width: 75%;"
+                                         class="responsive-image"
                                          alt="Logo MegaTraderX"/>
-
                                 </div>
 
                                 <div class="slider-content__info">
@@ -42,7 +41,7 @@ if (!isset($classes) || !is_string($classes)) $classes = '';
                             <div class="slider-content">
                                 <div class="slider-content__image-wrapper slider-content__image-wrapper--slider-2">
                                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/auth/sd2.png"
-                                         style="object-fit: fill;max-width: 75%;"
+                                         class="responsive-image"
                                          alt="Logo MegaTraderX"/>
                                 </div>
 
