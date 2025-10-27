38835f70d2e5f2777c3202c84ccb29e6a841de88
Luis Viera <levieraf@gmail.com>
2025-10-27 10:00:49 -0500

    adding changes on hero section

--- landing-page-bs.php
+++ landing-page-bs.php
@@ -22,13 +22,12 @@ get_header('landing-page-bs');
                             <li class="hero-bs__benefit d-flex align-items-start">
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                      xmlns="http://www.w3.org/2000/svg">
-                                    <mask id="mask0_17091_79292" style="mask-type:alpha" maskUnits="userSpaceOnUse"
-                                          x="0" y="0" width="24" height="24">
+                                    <mask id="mask0_17269_3092" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0"
+                                          y="0" width="24" height="24">
                                         <rect width="24" height="24" fill="#D9D9D9"/>
                                     </mask>
-                                    <g mask="url(#mask0_17091_79292)">
-                                        <path d="M10.6 16.6L17.65 9.55L16.25 8.15L10.6 13.8L7.75 10.95L6.35 12.35L10.6 16.6ZM12 22C10.6167 22 9.31667 21.7375 8.1 21.2125C6.88333 20.6875 5.825 19.975 4.925 19.075C4.025 18.175 3.3125 17.1167 2.7875 15.9C2.2625 14.6833 2 13.3833 2 12C2 10.6167 2.2625 9.31667 2.7875 8.1C3.3125 6.88333 4.025 5.825 4.925 4.925C5.825 4.025 6.88333 3.3125 8.1 2.7875C9.31667 2.2625 10.6167 2 12 2C13.3833 2 14.6833 2.2625 15.9 2.7875C17.1167 3.3125 18.175 4.025 19.075 4.925C19.975 5.825 20.6875 6.88333 21.2125 8.1C21.7375 9.31667 22 10.6167 22 12C22 13.3833 21.7375 14.6833 21.2125 15.9C20.6875 17.1167 19.975 18.175 19.075 19.075C18.175 19.975 17.1167 20.6875 15.9 21.2125C14.6833 21.7375 13.3833 22 12 22Z"
-                                              fill="#FFB34A"/>
+                                    <g mask="url(#mask0_17269_3092)">
+                                        <path d="M8 22L9 15H4L13 2H15L14 10H20L10 22H8Z" fill="#FFB34A"/>
                                     </g>
                                 </svg>
                                 <span class="hero-bs__text">Take a Challenge or get Instant Funding</span>
@@ -36,13 +35,12 @@ get_header('landing-page-bs');
                             <li class="hero-bs__benefit d-flex align-items-start">
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                      xmlns="http://www.w3.org/2000/svg">
-                                    <mask id="mask0_17091_79292" style="mask-type:alpha" maskUnits="userSpaceOnUse"
-                                          x="0" y="0" width="24" height="24">
+                                    <mask id="mask0_17269_3092" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0"
+                                          y="0" width="24" height="24">
                                         <rect width="24" height="24" fill="#D9D9D9"/>
                                     </mask>
-                                    <g mask="url(#mask0_17091_79292)">
-                                        <path d="M10.6 16.6L17.65 9.55L16.25 8.15L10.6 13.8L7.75 10.95L6.35 12.35L10.6 16.6ZM12 22C10.6167 22 9.31667 21.7375 8.1 21.2125C6.88333 20.6875 5.825 19.975 4.925 19.075C4.025 18.175 3.3125 17.1167 2.7875 15.9C2.2625 14.6833 2 13.3833 2 12C2 10.6167 2.2625 9.31667 2.7875 8.1C3.3125 6.88333 4.025 5.825 4.925 4.925C5.825 4.025 6.88333 3.3125 8.1 2.7875C9.31667 2.2625 10.6167 2 12 2C13.3833 2 14.6833 2.2625 15.9 2.7875C17.1167 3.3125 18.175 4.025 19.075 4.925C19.975 5.825 20.6875 6.88333 21.2125 8.1C21.7375 9.31667 22 10.6167 22 12C22 13.3833 21.7375 14.6833 21.2125 15.9C20.6875 17.1167 19.975 18.175 19.075 19.075C18.175 19.975 17.1167 20.6875 15.9 21.2125C14.6833 21.7375 13.3833 22 12 22Z"
-                                              fill="#FFB34A"/>
+                                    <g mask="url(#mask0_17269_3092)">
+                                        <path d="M8 22L9 15H4L13 2H15L14 10H20L10 22H8Z" fill="#FFB34A"/>
                                     </g>
                                 </svg>
                                 <span class="hero-bs__text">Lightning fast 1 hour payouts</span>
@@ -50,13 +48,12 @@ get_header('landing-page-bs');
                             <li class="hero-bs__benefit d-flex align-items-start">
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                      xmlns="http://www.w3.org/2000/svg">
-                                    <mask id="mask0_17091_79292" style="mask-type:alpha" maskUnits="userSpaceOnUse"
-                                          x="0" y="0" width="24" height="24">
+                                    <mask id="mask0_17269_3092" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0"
+                                          y="0" width="24" height="24">
                                         <rect width="24" height="24" fill="#D9D9D9"/>
                                     </mask>
-                                    <g mask="url(#mask0_17091_79292)">
-                                        <path d="M10.6 16.6L17.65 9.55L16.25 8.15L10.6 13.8L7.75 10.95L6.35 12.35L10.6 16.6ZM12 22C10.6167 22 9.31667 21.7375 8.1 21.2125C6.88333 20.6875 5.825 19.975 4.925 19.075C4.025 18.175 3.3125 17.1167 2.7875 15.9C2.2625 14.6833 2 13.3833 2 12C2 10.6167 2.2625 9.31667 2.7875 8.1C3.3125 6.88333 4.025 5.825 4.925 4.925C5.825 4.025 6.88333 3.3125 8.1 2.7875C9.31667 2.2625 10.6167 2 12 2C13.3833 2 14.6833 2.2625 15.9 2.7875C17.1167 3.3125 18.175 4.025 19.075 4.925C19.975 5.825 20.6875 6.88333 21.2125 8.1C21.7375 9.31667 22 10.6167 22 12C22 13.3833 21.7375 14.6833 21.2125 15.9C20.6875 17.1167 19.975 18.175 19.075 19.075C18.175 19.975 17.1167 20.6875 15.9 21.2125C14.6833 21.7375 13.3833 22 12 22Z"
-                                              fill="#FFB34A"/>
+                                    <g mask="url(#mask0_17269_3092)">
+                                        <path d="M8 22L9 15H4L13 2H15L14 10H20L10 22H8Z" fill="#FFB34A"/>
                                     </g>
                                 </svg>
                                 <span class="hero-bs__text">Journal to track, analyse and improve your trades</span>
@@ -66,12 +63,15 @@ get_header('landing-page-bs');
                         <div class="d-flex">
                             <a href="#get-funded" class="btn mega-btn-md mega-btn-primary-md hero-bs__btn">
                                 Get Funded Now
+                                <img src="<?= get_template_directory_uri() . '/assets/img/landing-page/flash-icon.svg'; ?>" width="44" height="43">
                             </a>
                         </div>
                     </div>
 
                     <div class="hero-bs__media">
-                        <div class="mt-card hero-bs__media-wrapper">
+                        <div class="hero-bs__media-wrapper">
+                            <img src="<?= get_template_directory_uri() . '/assets/img/landing-page/pork-bills.png' ?>"
+                                 alt="pork bills" width="532" height="522">
                         </div>
                     </div>
                 </div>
@@ -271,6 +271,9 @@ get_header('landing-page-bs');
                 </div>
             </div>
         </section>
+
+        <?php require 'template-parts/landing-page/sections/price_table.php'; ?>
+
     </main>
 
 <?php get_footer('landing-page-bs'); ?>
