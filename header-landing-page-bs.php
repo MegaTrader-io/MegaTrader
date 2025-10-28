07a7651f415056b0b6ae2cb98d676b705ebe1af6
Luis Viera <levieraf@gmail.com>
2025-10-28 18:11:36 -0500

    wip

--- header-landing-page-bs.php
+++ header-landing-page-bs.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class('font-roboto antialiased'); ?>>
