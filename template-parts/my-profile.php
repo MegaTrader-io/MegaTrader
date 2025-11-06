dc5267a333ea1c63a19a344b486dccd86138a1d7
Luis Viera <levieraf@gmail.com>
2025-11-06 18:13:46 -0500

    fix on my profile

--- template-parts/my-profile.php
+++ template-parts/my-profile.php
@@ -12,36 +12,39 @@ $logout_url = wp_logout_url();
 
 $collapsed = $args['collapsed'] ?? false;
 $collapsed_class = $collapsed ? 'collapsed' : '';
-$account_overview_url = profile_url(user_email: $user_profile['user_email']);
 
 ?>
 <div class="mt-my-profile <?= esc_attr($collapsed_class); ?>">
     <?php if ($go_to_dashboard): ?>
+        <?php
+        $account_overview_url = profile_url(user_email: $user_profile['user_email']);
+        ?>
         <a href="<?= esc_url($account_overview_url); ?>" class="mt-my-profile__avatar mt-my-profile__avatar_link">
             <?php get_template_part('template-parts/avatar', null, [
-                'avatar_url' => $user_profile['avatar_url'],
-                'avatar_size' => $user_profile['avatar_size'],
-                'has_real_avatar' => $user_profile['has_real_avatar'],
-                'initials' => $user_profile['initials'],
-                'display_name' => $user_profile['display_name'],
-                'user_email' => $user_profile['user_email'],
-                'billing_country' => $user_profile['billing_country'],
-                'hide_user_information' => $hide_user_information,
-                'collapsed' => $collapsed,
+                    'avatar_url' => $user_profile['avatar_url'],
+                    'avatar_size' => $user_profile['avatar_size'],
+                    'has_real_avatar' => $user_profile['has_real_avatar'],
+                    'initials' => $user_profile['initials'],
+                    'display_name' => $user_profile['display_name'],
+                    'user_email' => $user_profile['user_email'],
+                    'billing_country' => $user_profile['billing_country'],
+                    'hide_user_information' => $hide_user_information,
+                    'collapsed' => $collapsed,
             ]); ?>
         </a>
 
         <div class="my-account-logout">
             <a href="<?php echo esc_url($logout_url); ?>" class="logout-link" aria-label="Logout">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                    <mask id="mask0_12101_26191" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
-                        height="24">
-                        <rect width="24" height="24" fill="#D9D9D9" />
+                    <mask id="mask0_12101_26191" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0"
+                          width="24"
+                          height="24">
+                        <rect width="24" height="24" fill="#D9D9D9"/>
                     </mask>
                     <g mask="url(#mask0_12101_26191)">
                         <path
-                            d="M5 21C4.45 21 3.97917 20.8042 3.5875 20.4125C3.19583 20.0208 3 19.55 3 19V5C3 4.45 3.19583 3.97917 3.5875 3.5875C3.97917 3.19583 4.45 3 5 3H12V5H5V19H12V21H5ZM16 17L14.625 15.55L17.175 13H9V11H17.175L14.625 8.45L16 7L21 12L16 17Z"
-                            fill="white" />
+                                d="M5 21C4.45 21 3.97917 20.8042 3.5875 20.4125C3.19583 20.0208 3 19.55 3 19V5C3 4.45 3.19583 3.97917 3.5875 3.5875C3.97917 3.19583 4.45 3 5 3H12V5H5V19H12V21H5ZM16 17L14.625 15.55L17.175 13H9V11H17.175L14.625 8.45L16 7L21 12L16 17Z"
+                                fill="white"/>
                     </g>
                 </svg>
             </a>
@@ -50,22 +53,23 @@ $account_overview_url = profile_url(user_email: $user_profile['user_email']);
 
         <div class="mt-my-profile__avatar">
             <?php get_template_part('template-parts/avatar', null, [
-                'avatar_url' => $user_profile['avatar_url'],
-                'avatar_size' => $user_profile['avatar_size'],
-                'has_real_avatar' => $user_profile['has_real_avatar'],
-                'initials' => $user_profile['initials'],
-                'display_name' => $user_profile['display_name'],
-                'user_email' => $user_profile['user_email'],
-                'billing_country' => $user_profile['billing_country'],
-                'collapsed' => $collapsed,
+                    'avatar_url' => $user_profile['avatar_url'],
+                    'avatar_size' => $user_profile['avatar_size'],
+                    'has_real_avatar' => $user_profile['has_real_avatar'],
+                    'initials' => $user_profile['initials'],
+                    'display_name' => $user_profile['display_name'],
+                    'user_email' => $user_profile['user_email'],
+                    'billing_country' => $user_profile['billing_country'],
+                    'collapsed' => $collapsed,
             ]); ?>
         </div>
 
 
         <div class="mt-my-profile__notifications" data-mt-ajax="<?php echo esc_url(admin_url('admin-ajax.php')); ?>"
-            data-mt-nonce="<?php echo esc_attr(wp_create_nonce('mt_user_notifs')); ?>">
-            <a id="mt-notifications-toggle" class="mt-sidebar__menu__link" href="javascript:void(0)" aria-haspopup="dialog"
-                aria-expanded="false">
+             data-mt-nonce="<?php echo esc_attr(wp_create_nonce('mt_user_notifs')); ?>">
+            <a id="mt-notifications-toggle" class="mt-sidebar__menu__link" href="javascript:void(0)"
+               aria-haspopup="dialog"
+               aria-expanded="false">
                 <i class="mt-icon mt-icon_notifications"></i>
             </a>
 
@@ -74,9 +78,9 @@ $account_overview_url = profile_url(user_email: $user_profile['user_email']);
             $mt_user_email = $user_profile['user_email'] ?? '';
 
             get_template_part(
-                'template-parts/account/account-notification',
-                null,
-                ['user_email' => $mt_user_email]
+                    'template-parts/account/account-notification',
+                    null,
+                    ['user_email' => $mt_user_email]
             );
             ?>
         </div>
