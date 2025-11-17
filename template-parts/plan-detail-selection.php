815c0d608056adde46e2a5ec212846e6a1766fff
Luis Viera <levieraf@gmail.com>
2025-11-17 17:41:23 -0500

    cambios varios en landing page y en auth page

--- template-parts/plan-detail-selection.php
+++ template-parts/plan-detail-selection.php
@@ -88,9 +88,6 @@ HTML;
                 <img class="plan-detail-selection__account-icon"
                      src="<?= esc_url($account_thumbnail_url); ?>"
                      alt="Plan Icon">
-                <img class="plan-detail-selection__platform-icon"
-                     src="<?= esc_url($platform_url); ?>"
-                     alt="Platform Icon">
 
                 <div class="plan-detail-selection__name">
                   <?= strtoupper($account_size . ' ' . $account_type['name']); ?>
