12dd296e16d3b97e78e13d0e3ebb2b71098a90ad
Luis Viera <levieraf@gmail.com>
2025-11-21 13:36:50 -0500

    adicion de estilos para iconos de platforms

--- landing-page-bs.php
+++ landing-page-bs.php
@@ -203,22 +203,26 @@ get_header('landing-page-bs');
         ?>
 
         <section class="platforms-bs">
-            <div class="container">
+            <div class="landing-bs-container">
                 <div class="platforms-bs__list">
-                    <div class="platforms-bs__item platforms-bs__item--available">
-                        <img src="<?= $platform_base_url . '/megatraderx.svg'; ?>" alt="MegaTraderX Sponsor Logo">
+                    <div class="platforms-bs__first-row">
+                        <div class="platforms-bs__item platforms-bs__item--available">
+                            <img src="<?= $platform_base_url . '/megatraderx.svg'; ?>" alt="MegaTraderX Sponsor Logo">
+                        </div>
+                        <div class="platforms-bs__item">
+                            <img src="<?= $platform_base_url . '/tradovate.svg'; ?>" alt="MegaTraderX Sponsor Logo">
+                            <div class="mt-badge mt-badge-rounded-sm mt-badge-light">COMING SOON</div>
+                        </div>
                     </div>
-                    <div class="platforms-bs__item">
-                        <img src="<?= $platform_base_url . '/tradovate.svg'; ?>" alt="MegaTraderX Sponsor Logo">
-                        <div class="mt-badge mt-badge-rounded-sm mt-badge-light">COMING SOON</div>
-                    </div>
-                    <div class="platforms-bs__item">
-                        <img src="<?= $platform_base_url . '/ninjatrader.svg'; ?>" alt="MegaTraderX Sponsor Logo">
-                        <div class="mt-badge mt-badge-rounded-sm mt-badge-light">COMING SOON</div>
-                    </div>
-                    <div class="platforms-bs__item">
-                        <img src="<?= $platform_base_url . '/quantower.svg'; ?>" alt="MegaTraderX Sponsor Logo">
-                        <div class="mt-badge mt-badge-rounded-sm mt-badge-light">COMING SOON</div>
+                    <div class="platforms-bs__second-row">
+                        <div class="platforms-bs__item">
+                            <img src="<?= $platform_base_url . '/ninjatrader.svg'; ?>" alt="MegaTraderX Sponsor Logo">
+                            <div class="mt-badge mt-badge-rounded-sm mt-badge-light">COMING SOON</div>
+                        </div>
+                        <div class="platforms-bs__item">
+                            <img src="<?= $platform_base_url . '/quantower.svg'; ?>" alt="MegaTraderX Sponsor Logo">
+                            <div class="mt-badge mt-badge-rounded-sm mt-badge-light">COMING SOON</div>
+                        </div>
                     </div>
                 </div>
             </div>
