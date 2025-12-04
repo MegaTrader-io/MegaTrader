72015484ca866d0135062d92b06983b8771d565a
Luis Viera <levieraf@gmail.com>
2025-12-03 23:15:38 -0500

    wip

--- assets/js/landing-page-bs.js
+++ assets/js/landing-page-bs.js
@@ -143,7 +143,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
                         // 🔹 Ejemplo: cambiar autoplay dinámicamente
                         if (document.documentElement.clientWidth <= 768 && !Glide.settings.autoplay) {
-                            Glide.update({autoplay: 3000, type: 'slider', focusAt: 'center'});
+                            Glide.update({ focusAt: 'center'});
                         } else if (document.documentElement.clientWidth > 768 && Glide.settings.autoplay) {
                             Glide.update({autoplay: false});
                         }
