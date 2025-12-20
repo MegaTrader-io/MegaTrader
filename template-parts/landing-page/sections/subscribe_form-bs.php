96bbae6b5582c43871d3146dc7da6d5d15dabc95
Luis Viera <levieraf@gmail.com>
2025-12-19 23:42:25 -0500

    adding footer wip

---
+++ template-parts/landing-page/sections/subscribe_form-bs.php
@@ -0,0 +1,33 @@
+<form novalidate class="footer-bs__column footer-bs__column--subscribe">
+    <div class="footer-bs__alert" style="display: none">
+        <div role="alert" tabindex="-1" class="woocommerce-message">
+            Congratulations, you have successfully subscribed.
+        </div>
+    </div>
+
+    <div class="footer-bs__form">
+        <div class="footer-bs__input-group">
+            <label for="email" class="w-100 mb-0">
+                <input type="email" id="email" name="email" placeholder="Enter your email" class="form-control">
+            </label>
+        </div>
+
+        <button disabled class="btn mega-btn-md mega-btn-primary-md">
+            SUBSCRIBE
+        </button>
+    </div>
+
+    <div class="footer-bs__consent">
+        <div class="checkbox-container">
+            <input class="checkbox-container__input" type="checkbox"
+                   id="email_consent"
+                   name="email_consent">
+
+            <label for="email_consent" class="checkbox-container__label m-0">
+                <span class="footer-bs__consent-text">
+                    I consent to the use of my email address to receive news, updates, and important notifications.
+                </span>
+            </label>
+        </div>
+    </div>
+</form>
