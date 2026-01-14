0930271601b26f394bc3ba6117f6e6438d8b7dfa
Luis Viera <levieraf@gmail.com>
2026-01-14 13:30:21 -0500

    zero-plan

---
+++ header-landing-page-zero-plan.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<?php error_log('[LV] after language_attributes'); ?>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class('font-roboto antialiased'); ?>>
+<?php wp_body_open(); ?>
+<div class="preloader" style="display: block;">
+    <div class="preloader-inner">
+        <span class="loader"></span>
+    </div>
+</div>
+<?php Mt_Navbar::render_navbar_bs(
+        section: 'landing-page-zero-plan',
+        classes_navbar: 'mt-navbar--landing-page-bs mt-navbar__links--scrolled'
+); ?>
--- landing-page-zero-plan.php
+++ landing-page-zero-plan.php
@@ -3,7 +3,7 @@
  * Template Name: Landing Page Zero Plan
  */
 
-get_header('landing-page-bs');
+get_header('landing-page-zero-plan');
 ?>
     <main class="landing-bs">
         <?php get_template_part('template-parts/landing-page/sections/hero-bs'); ?>
